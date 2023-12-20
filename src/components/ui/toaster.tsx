ed9608fb3d85f3ba75384a4d5e8b1f1977738718
Vigee <contact@vigee.fr>
2023-12-20 09:58:14 +0100

    added ui emelements in shadcn

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,4 +1,5 @@
-"use client";
+"use client"
+
 import {
   Toast,
   ToastClose,
@@ -6,11 +7,11 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "./toast";
-import { useToast } from "./use-toast";
+} from "@/app/components/ui/toast"
+import { useToast } from "@/app/components/ui/use-toast"
 
 export function Toaster() {
-  const { toasts } = useToast();
+  const { toasts } = useToast()
 
   return (
     <ToastProvider>
@@ -26,9 +27,9 @@ export function Toaster() {
             {action}
             <ToastClose />
           </Toast>
-        );
+        )
       })}
       <ToastViewport />
     </ToastProvider>
-  );
+  )
 }
