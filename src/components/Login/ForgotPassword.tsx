f94dd31585ca4117da3537ea31d4b10331d4cbc8
Vigee <contact@vigee.fr>
2023-12-22 15:27:12 +0100

    added responsive

--- src/components/Login/ForgotPassword.tsx
+++ src/components/Login/ForgotPassword.tsx
@@ -48,15 +48,15 @@ export default function ForgotPassword({ logo }: Props) {
 
   return (
     <>
-      <div className="w-full max-w-xl space-y-6 align-center my-auto justify-start mx-auto py-40 px-8 md:p-24 md:py-4 min-h-screen md:min-h-fit ">
+      <div className="w-full max-w-xl space-y-6 align-center my-auto justify-start mx-auto py-40 md:px-8 md:p-24 md:py-4 min-h-screen md:min-h-fit md:max-w-[480px]">
         <Image
           width={100}
           height={100}
-          className=" mx-auto"
+          className=" ml-4 md:ml-4 absolute md:relative top-12 md:top-0"
           src={logo}
           alt="Vigee - Make IT Simple"
         />
-        <div className="flex flex-col justify-start mx-auto shadow-sm border border-gray-100 rounded-xl p-8 bg-white ">
+        <div className="flex flex-col justify-start mx-auto md:shadow-sm  md:border border-gray-100 rounded-xl px-4 md:p-8  md:bg-white">
           <div>
             <TypographyH1 className="text-primary pb-4">
               Réinitialiser mon mot de passe
@@ -80,21 +80,26 @@ export default function ForgotPassword({ logo }: Props) {
                 )}
               />
 
-              <Button type="submit">Réinitialiser mon mot de passe</Button>
+              <Link
+                href="/login"
+                className="font-base hover:font-bold text-sm hover:text-primary flex gap-x-2 text-gray-400 transform hover:scale-105 transition duration-300 ease-in-out"
+              >
+                <ArrowLongLeftIcon width={20} />
+                Retour à la connexion
+              </Link>
+              <div className="flex flex-col items-center ">
+                <div className="absolute md:relative bottom-12 md:bottom-0 w-full px-4 md:px-0 items-center gap-2 ">
+                  <Button
+                    type="submit"
+                    className="w-full h-12 text-md font-bold "
+                  >
+                    Réinitialiser mon mot de passe
+                  </Button>
+                </div>
+              </div>
             </form>
           </Form>
-
-          <div className="flex items-center pt-2">
-            <Link
-              href="/login"
-              className="font-base hover:font-bold text-sm hover:text-primary flex gap-x-2 text-gray-400 transform hover:scale-105 transition duration-300 ease-in-out"
-            >
-              <ArrowLongLeftIcon width={20} />
-              Retour à la connexion
-            </Link>
-          </div>
         </div>
-        <Copyright />
       </div>
     </>
   );
