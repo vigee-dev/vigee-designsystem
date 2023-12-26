9558c12f396ab91cc3f9a3a35707bdd7b29dfd14
Vigee <contact@vigee.fr>
2023-12-26 16:00:08 +0100

    new sales list type

---
+++ components/Vigee/ByVigee.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import Image from "next/image";
+import vigee from "../../img/logos/vigee.png";
+import React from "react";
+
+interface ByVigeeProps {
+  dark: boolean;
+}
+
+const ByVigee: React.FC<ByVigeeProps> = ({ dark }) => {
+  return dark ? (
+    <span className="text-slate-400 flex pt-8">
+      <Link
+        href="https://www.vigee.fr"
+        target="_blank"
+        className="text-slate-400 flex"
+      >
+        Developed by{" "}
+        <Image
+          src={vigee}
+          width={70}
+          height={20}
+          alt="Vigee"
+          className="pt-1 ml-2 "
+        />
+      </Link>
+    </span>
+  ) : (
+    <span className="text-primary flex">
+      <Link
+        href="https://www.vigee.fr"
+        target="_blank"
+        className="text-primary flex"
+      >
+        Developed by{" "}
+        <Image
+          src={vigee}
+          width={70}
+          height={20}
+          alt="Vigee"
+          className="pt-1 ml-2 "
+        />
+      </Link>
+    </span>
+  );
+};
+
+export default ByVigee;
