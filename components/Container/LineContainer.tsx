4f2efa00cbb3a56c0b47f0a34024acc39c114364
Vigee <contact@vigee.fr>
2023-12-27 12:26:46 +0100

    icons

--- components/Container/LineContainer.tsx
+++ components/Container/LineContainer.tsx
@@ -3,14 +3,15 @@ import { TypographyH3, TypographyH4 } from "../Typography/Typography";
 interface PageHeaderProps {
   title: string;
   children?: React.ReactNode;
-  small?: boolean;
   background?: string;
+  icon?: React.ReactNode;
 }
 
 export function LineContainer({
   title,
   children,
   background,
+  icon,
 }: PageHeaderProps) {
   return (
     <div
@@ -19,6 +20,8 @@ export function LineContainer({
       }`}
     >
       <div className="flex flex-wrap justify-between  gap-x-4 w-full items-center ">
+        {icon && <div className="icon-container">{icon}</div>}{" "}
+        {/* Affichage de l'icône */}
         <TypographyH4>{title}</TypographyH4>
         <div className="flex gap-x-4">{children}</div>
       </div>
