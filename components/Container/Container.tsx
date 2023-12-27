874a44cef75afc73c205da5a742c78a5e1fa6481
Vigee <contact@vigee.fr>
2023-12-27 12:19:59 +0100

    icons

--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export function Container({ children }: Props) {
   return (
-    <div className="bg-white flex border rounded-md shadow-sm p-8 max-w-7xl justify-center mx-auto my-4">
+    <div className="bg-white flex flex-col border rounded-md shadow-sm p-8 max-w-7xl justify-center mx-auto my-4">
       {children}
     </div>
   );
--- components/Container/LineContainer.tsx
+++ components/Container/LineContainer.tsx
@@ -4,11 +4,18 @@ interface PageHeaderProps {
   title: string;
   children?: React.ReactNode;
   small?: boolean;
+  background?: string;
 }
 
-export function LineContainer({ title, children, small }: PageHeaderProps) {
+export function LineContainer({
+  title,
+  children,
+  background,
+}: PageHeaderProps) {
   return (
-    <div className="rounded-md h-fit bg-white p-5  border mb-4 items-center ">
+    <div
+      className={`rounded-md h-fit p-5  border mb-4 items-center  bg-${background}`}
+    >
       <div className="flex flex-wrap justify-between  gap-x-4 w-full items-center ">
         <TypographyH4>{title}</TypographyH4>
         <div className="flex gap-x-4">{children}</div>
