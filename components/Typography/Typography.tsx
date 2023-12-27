6a0d85210619af15bb9eb00e3cbfa3bffee13c09
Vigee <contact@vigee.fr>
2023-12-27 12:16:54 +0100

    icons

--- components/Typography/Typography.tsx
+++ components/Typography/Typography.tsx
@@ -49,11 +49,7 @@ export function TypographyH3({
 }
 
 export function TypographyH4({ children }: { children: React.ReactNode }) {
-  return (
-    <h4 className="scroll-m-20 text-md font-semibold tracking-tight">
-      {children}
-    </h4>
-  );
+  return <h4 className="scroll-m-20 text-md  tracking-tight">{children}</h4>;
 }
 
 export function TypographyBlockquote({
