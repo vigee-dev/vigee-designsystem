b938759a7e02f6f2c6c210fceb7d4623b1c15656
Vigee <contact@vigee.fr>
2023-12-27 10:54:32 +0100

    better page header sizing

--- components/Headers/PageHeader.tsx
+++ components/Headers/PageHeader.tsx
@@ -1,16 +1,21 @@
 import React from "react";
-import { TypographyH1 } from "../Typography/Typography";
+import { TypographyH1, TypographyH2 } from "../Typography/Typography";
 
 interface PageHeaderProps {
   title: string;
   children?: React.ReactNode;
+  small?: boolean;
 }
 
-export function PageHeader({ title, children }: PageHeaderProps) {
+export function PageHeader({ title, children, small }: PageHeaderProps) {
   return (
     <div className=" rounded-md h-fit bg-white p-5  border mb-4">
       <div className="flex flex-wrap justify-between  gap-x-4 w-full items-center ">
-        <TypographyH1>{title}</TypographyH1>
+        {small ? (
+          <TypographyH2>{title}</TypographyH2>
+        ) : (
+          <TypographyH1>{title}</TypographyH1>
+        )}
         <div className="flex gap-x-4">{children}</div>
       </div>
     </div>
