9bb5fa76c7802f5a511b6b1c92626041d647724f
Vigee <contact@vigee.fr>
2023-12-28 18:35:31 +0100

    dialog on desktop

--- components/Headers/PageHeader.tsx
+++ components/Headers/PageHeader.tsx
@@ -13,12 +13,13 @@ export function PageHeader({ title, children, small, icon }: PageHeaderProps) {
     <div className=" rounded-md h-fit bg-white p-5  border mb-4 items-center ">
       <div className="flex flex-wrap justify-between  gap-x-4 w-full items-center ">
         <div className="flex items-center gap-4 ">
+          {" "}
+          {icon}
           {small ? (
             <TypographyH2>{title}</TypographyH2>
           ) : (
             <TypographyH1>{title}</TypographyH1>
           )}
-          {icon}
         </div>
         <div className="flex gap-x-4">{children}</div>
       </div>
