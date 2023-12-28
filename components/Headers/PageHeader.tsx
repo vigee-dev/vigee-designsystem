bb3774d6e2ff66e8e98ec2b2ecd83c42e55bc423
Vigee <contact@vigee.fr>
2023-12-28 18:34:39 +0100

    dialog on desktop

--- components/Headers/PageHeader.tsx
+++ components/Headers/PageHeader.tsx
@@ -5,17 +5,21 @@ interface PageHeaderProps {
   title: string;
   children?: React.ReactNode;
   small?: boolean;
+  icon?: React.ReactNode;
 }
 
-export function PageHeader({ title, children, small }: PageHeaderProps) {
+export function PageHeader({ title, children, small, icon }: PageHeaderProps) {
   return (
     <div className=" rounded-md h-fit bg-white p-5  border mb-4 items-center ">
       <div className="flex flex-wrap justify-between  gap-x-4 w-full items-center ">
-        {small ? (
-          <TypographyH2>{title}</TypographyH2>
-        ) : (
-          <TypographyH1>{title}</TypographyH1>
-        )}
+        <div className="flex items-center gap-4 ">
+          {small ? (
+            <TypographyH2>{title}</TypographyH2>
+          ) : (
+            <TypographyH1>{title}</TypographyH1>
+          )}
+          {icon}
+        </div>
         <div className="flex gap-x-4">{children}</div>
       </div>
     </div>
