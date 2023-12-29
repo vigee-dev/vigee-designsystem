dfca138f899237a92c432f638f9449b028730ae7
Vigee <contact@vigee.fr>
2023-12-29 11:25:23 +0100

    resizable

--- icons/NewsPaper.tsx
+++ icons/NewsPaper.tsx
@@ -1,11 +1,11 @@
-export const NewsPaperIcon = ({ w = 6, h = 6 }) => (
+export const NewsPaperIcon = ({ w = 6, h = 6, className = "" }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
     stroke-width="1.5"
     stroke="currentColor"
-    className={`h-${h} w-${w}`}
+    className={`h-${h} w-${w} ${className}`}
   >
     <path
       stroke-linecap="round"
