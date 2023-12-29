11aecc103ba0ec372a376ed8428af06d9f99ffd3
Vigee <contact@vigee.fr>
2023-12-29 10:46:19 +0100

    resizable

--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -2,11 +2,14 @@ import React from "react";
 
 interface Props {
   children: React.ReactNode;
+  className?: string;
 }
 
-export function Container({ children }: Props) {
+export function Container({ children, className }: Props) {
   return (
-    <div className="bg-white flex flex-col border rounded-md shadow-sm  justify-center  mb-4 w-full py-5">
+    <div
+      className={`bg-white flex flex-col border rounded-md shadow-sm  justify-center  mb-4 w-full  ${className}`}
+    >
       {children}
     </div>
   );
