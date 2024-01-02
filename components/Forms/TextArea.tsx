dea2f47525f1fc40fde21174e74aff05f844a1d0
Vigee <contact@vigee.fr>
2024-01-02 08:58:48 +0100

    TextArea

--- components/Forms/TextArea.tsx
+++ components/Forms/TextArea.tsx
@@ -3,22 +3,40 @@ import { Textarea } from "../ui/textarea";
 import { Label } from "../ui/label";
 import { useState } from "react";
 
-interface TextAreaVigeeProps {
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { UseFormReturn, FieldValues, Path } from "react-hook-form";
+import { z } from "zod";
+
+type Props<T extends z.ZodType<any, any>> = {
+  form: UseFormReturn<z.infer<T> & FieldValues>;
   id?: string;
-  name: string;
+  name: Path<z.infer<T> & FieldValues>;
   label?: string;
+  required?: boolean;
   placeholder?: string;
   count?: boolean;
   max?: number;
+  descr?: string;
   defaultValue?: string;
+  className?: string;
   minHeight?: string;
   onChange?: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
   onBlur?: (e: React.FocusEvent<HTMLTextAreaElement>) => void;
-}
+};
 
-export const TextArea = ({
+export default function TextArea<T extends z.ZodType<any, any, any>>({
+  form,
   id,
   name,
+  required,
   label,
   placeholder,
   count,
@@ -27,40 +45,40 @@ export const TextArea = ({
   defaultValue,
   onBlur,
   onChange,
-}: TextAreaVigeeProps) => {
+  className,
+  descr,
+}: Props<T>) {
   const [charCount, setCharCount] = useState(0); // État local pour le compteur de caractères
 
   return (
-    <div className="w-full my-2">
-      <div className="flex justify-between items-center ">
-        {label && (
-          <Label htmlFor={id} className="mb-2">
-            {label}
-          </Label>
-        )}
-        {count && (
-          <span className="text-xs text-gray-500">
-            {charCount} / {max}
-          </span>
-        )}
-      </div>
-      <Textarea
-        name={name}
-        id={id}
-        placeholder={placeholder ?? ""}
-        onChange={(e) => {
-          if (count) {
-            setCharCount(e.target.value.length); // Mise à jour du compteur de caractères
-          }
-          onChange && onChange(e);
-        }}
-        maxLength={max}
-        defaultValue={defaultValue ?? ""}
-        className={`min-h-${minHeight || "10"} h-${
-          minHeight || "10"
-        } resize-none `}
-        onBlur={onBlur}
-      />
-    </div>
+    <FormField
+      control={form.control}
+      name={name}
+      rules={{ required }}
+      render={({ field }) => (
+        <FormItem className={className}>
+          <FormLabel className="font-black text-primary">{label}</FormLabel>
+          <Textarea
+            name={name}
+            id={id}
+            placeholder={placeholder ?? ""}
+            onChange={(e) => {
+              if (count) {
+                setCharCount(e.target.value.length); // Mise à jour du compteur de caractères
+              }
+              onChange && onChange(e);
+            }}
+            maxLength={max}
+            defaultValue={defaultValue ?? ""}
+            className={`min-h-${minHeight || "10"} h-${
+              minHeight || "10"
+            } resize-none `}
+            onBlur={onBlur}
+          />
+          {descr && <FormDescription>{descr}</FormDescription>}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
   );
-};
+}
