e50de49ac965d2e38f03acef318694ecb84394a8
Vigee <contact@vigee.fr>
2024-01-02 22:27:35 +0100

    pika icons

--- icons/PikaIcons.tsx
+++ icons/PikaIcons.tsx
@@ -198,3 +198,23 @@ export function PhonePhone(props: JSX.IntrinsicElements["svg"]) {
     </svg>
   );
 }
+
+export function CalendarCheck(props: JSX.IntrinsicElements["svg"]) {
+  return (
+    <svg
+      {...props}
+      style={{ color: "888" }}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    >
+      <path d="M16 2v2.128m0 0V6m0-1.872C15.059 4 13.828 4 12 4c-1.828 0-3.059 0-4 .128m8 0c.498.067.915.17 1.296.329a6 6 0 0 1 3.247 3.247C21 8.807 21 10.204 21 13c0 2.796 0 4.194-.457 5.296a6 6 0 0 1-3.247 3.247C16.194 22 14.796 22 12 22c-2.796 0-4.193 0-5.296-.457a6 6 0 0 1-3.247-3.247C3 17.194 3 15.796 3 13c0-2.796 0-4.193.457-5.296a6 6 0 0 1 3.247-3.247A5.135 5.135 0 0 1 8 4.127M8 2v2.128m0 0V6m.75 7.922 2.174 2.172a13.915 13.915 0 0 1 4.232-4.584l.094-.064" />
+    </svg>
+  );
+}
