2bf4bb7f6c9907a713689d5582ad28548fb1b3e4
Vigee <contact@vigee.fr>
2024-01-02 21:58:31 +0100

    pika icons

--- icons/User.tsx
+++ icons/User.tsx
@@ -51,3 +51,23 @@ export function UserCircular(props: JSX.IntrinsicElements["svg"]) {
     </svg>
   );
 }
+
+export function User2(props: JSX.IntrinsicElements["svg"]) {
+  return (
+    <svg
+      {...props}
+      style={{ color: "010" }}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    >
+      <path d="M17 3.535c1.196.692 2 1.984 2 3.465 0 1.48-.804 2.773-2 3.465m4 10.268A2 2 0 0 0 22 19c0-1.48-.804-2.773-2-3.465M14 7a4 4 0 1 1-8 0 4 4 0 0 1 8 0Zm-8 8h8a4 4 0 0 1 4 4 2 2 0 0 1-2 2H4a2 2 0 0 1-2-2 4 4 0 0 1 4-4Z" />
+    </svg>
+  );
+}
