ae081655c9fde8300e72ffbead50ff73c8f87795
Vigee <contact@vigee.fr>
2024-01-02 22:34:12 +0100

    pika icons

--- icons/PikaIcons.tsx
+++ icons/PikaIcons.tsx
@@ -219,3 +219,23 @@ export function CalendarCheck(props: JSX.IntrinsicElements["svg"]) {
     </svg>
   );
 }
+
+export function UserPlus(props: JSX.IntrinsicElements["svg"]) {
+  return (
+    <svg
+      {...props}
+      style={{ color: "#94a3b8" }}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    >
+      <path d="M11 15H7a4 4 0 0 0-4 4 2 2 0 0 0 2 2h10m3-3v-3m0 0v-3m0 3h-3m3 0h3m-6-8a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z" />
+    </svg>
+  );
+}
