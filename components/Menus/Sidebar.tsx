52fff141c7582e56fcfbc584a4d1aa76057fba71
Vigee <contact@vigee.fr>
2024-01-03 12:18:57 +0100

    new icons all

--- components/Menus/Sidebar.tsx
+++ components/Menus/Sidebar.tsx
@@ -154,7 +154,7 @@ const Sidebar: React.FC<SidebarProps> = ({
                                 className={classNames(
                                   `${
                                     text === "white"
-                                      ? "text-gray-100 group-hover:text-gray-100"
+                                      ? "text-gray-300 group-hover:text-gray-300"
                                       : "text-primary group-hover:text-primary"
                                   } group-hover:scale-105 transform transition-all duration-300 ease-in-out`,
                                   `h-${"8"} w-${"8"} shrink-0  my-auto ml-1  p-[5px]`
