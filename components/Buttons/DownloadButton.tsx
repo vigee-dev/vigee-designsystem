1ea92eb0ffe417eda22fb39258ff24dcf0be6daa
Vigee <contact@vigee.fr>
2024-01-03 16:46:17 +0100

    new icons all

--- components/Buttons/DownloadButton.tsx
+++ components/Buttons/DownloadButton.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button } from "./Button";
-import { PiCloudArrowDownloadStroke } from "../../icons/pikaicons";
+import { PiCloudArrowDownloadStroke } from "../../icons/PikaIcons";
 
 interface DownloadButtonProps {
   onClick?: () => void;
