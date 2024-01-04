2e0ef9c9be0a8056f85cd7cb1b53cd78e0f5fb89
Vigee <contact@vigee.fr>
2024-01-04 22:40:12 +0100

    context

--- components/Forms/DrawerMobile.tsx
+++ components/Forms/DrawerMobile.tsx
@@ -34,14 +34,23 @@ interface Props {
   setOpenForced?: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-// SET THE FORM AS CHILDREN
-// SEND A BUTTON IN YOUR PAGE AS TRIGGER
-{
-  /* <DrawerMobile
-title="Titre"
-description="Bonjour"
-trigger={<Button variant="outline">Nouveau</Button>}
-> {children} </DrawerMobile>*/
+type DrawerContextType = {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const DrawerContext = React.createContext<DrawerContextType | undefined>(
+  undefined
+);
+
+export function useDrawerContext() {
+  const context = React.useContext(DrawerContext);
+  if (context === undefined) {
+    throw new Error(
+      "useDrawerContext doit être utilisé à l'intérieur d'un DrawerContext.Provider"
+    );
+  }
+  return context;
 }
 
 export function DrawerMobile({
@@ -51,57 +60,62 @@ export function DrawerMobile({
   trigger,
   icon,
   disabled,
-  openForced,
-  setOpenForced,
 }: Props) {
   const [open, setOpen] = React.useState(false);
+
+  const contextValue: DrawerContextType = {
+    open,
+    setOpen,
+  };
+
   const isDesktop = useMediaQuery("(min-width: 768px)");
+  const DrawerContext = React.createContext<DrawerContextType | undefined>(
+    undefined
+  );
 
   if (isDesktop) {
     return (
-      <Dialog
-        open={openForced ? openForced : open}
-        onOpenChange={setOpenForced ? setOpenForced : setOpen}
-      >
-        <DialogTrigger asChild>{trigger}</DialogTrigger>
-        <DialogContent className="sm:max-w-[425px] ">
-          <DialogHeader>
-            <div className="flex items-center gap-4 ">
-              {icon}
-              <div className="flex flex-col ">
-                <DialogTitle>{title}</DialogTitle>
-                <DialogDescription>{description}</DialogDescription>
+      <DrawerContext.Provider value={contextValue}>
+        <Dialog open={open} onOpenChange={setOpen}>
+          <DialogTrigger asChild>{trigger}</DialogTrigger>
+          <DialogContent className="sm:max-w-[425px] ">
+            <DialogHeader>
+              <div className="flex items-center gap-4 ">
+                {icon}
+                <div className="flex flex-col ">
+                  <DialogTitle>{title}</DialogTitle>
+                  <DialogDescription>{description}</DialogDescription>
+                </div>
               </div>
-            </div>
-          </DialogHeader>
-          {children}
-        </DialogContent>
-      </Dialog>
+            </DialogHeader>
+            {children}
+          </DialogContent>
+        </Dialog>
+      </DrawerContext.Provider>
     );
   }
 
   return (
-    <Drawer
-      open={openForced ? openForced : open}
-      onOpenChange={setOpenForced ? setOpenForced : setOpen}
-    >
-      <DrawerTrigger asChild>{trigger}</DrawerTrigger>
-      <DrawerContent>
-        <DrawerHeader className="text-left">
-          <DrawerTitle>{title}</DrawerTitle>
-          <DrawerDescription>{description}</DrawerDescription>
-        </DrawerHeader>
+    <DrawerContext.Provider value={contextValue}>
+      <Drawer open={open} onOpenChange={setOpen}>
+        <DrawerTrigger asChild>{trigger}</DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader className="text-left">
+            <DrawerTitle>{title}</DrawerTitle>
+            <DrawerDescription>{description}</DrawerDescription>
+          </DrawerHeader>
 
-        <div className="px-4">{children}</div>
+          <div className="px-4">{children}</div>
 
-        <DrawerFooter>
-          <DrawerClose asChild>
-            <Button type="button" variant="outline">
-              Annuler
-            </Button>
-          </DrawerClose>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
+          <DrawerFooter>
+            <DrawerClose asChild>
+              <Button type="button" variant="outline">
+                Annuler
+              </Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </DrawerContext.Provider>
   );
 }
