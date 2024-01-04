f9debe34d373b4ac1e3ef52ebfba0c46f28dc032
Vigee <contact@vigee.fr>
2024-01-04 11:10:37 +0100

    new login compnents colors

--- components/Loaders/Loader.tsx
+++ components/Loaders/Loader.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import VigeeLoaderTransparent from "../../img/logos/VigeeGrayLogo.png";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
 interface Props {
   text?: String;
+  logo?: StaticImageData;
 }
 
-export const Loader = ({ text }: Props) => {
+export const Loader = ({ text, logo }: Props) => {
   return (
     <div className="flex flex-col justify-center items-center gap-y-2 p-12 h-screen">
       <div className="relative flex items-center justify-center">
         <div className="absolute h-28 w-28 border-2 border-gray-300 rounded-full animate-spin border-t-transparent" />
 
         <Image
-          src={VigeeLoaderTransparent}
+          src={logo ? logo : VigeeLoaderTransparent}
           alt="logo"
           width={50}
           height={40}
