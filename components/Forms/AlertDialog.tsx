17fd3a5538907f3cfd316cc3de3a8b1c987c98d5
Vigee <contact@vigee.fr>
2024-01-05 02:21:15 +0100

    alert dialog with context

---
+++ components/Forms/AlertDialog.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import {
+  AlertDialog as Alert,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../../components/ui/alert-dialog";
+import { Button } from "../../components/ui/button";
+
+type Props = {
+  btnText: string;
+  btnQuestion: string;
+  btnSubAlert?: string;
+  onClick: () => void;
+  colorBtn?: "outline" | "destructive";
+};
+
+type DrawerContextType = {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const DrawerContext = React.createContext<DrawerContextType | undefined>(
+  undefined
+);
+
+export function useAlertContext() {
+  const context = React.useContext(DrawerContext);
+  if (context === undefined) {
+    throw new Error(
+      "useAlertContext doit être utilisé à l'intérieur d'un useAlertContext.Provider"
+    );
+  }
+  return context;
+}
+
+export function AlertDialog({
+  btnText,
+  btnQuestion,
+  btnSubAlert,
+  colorBtn = "outline",
+  onClick,
+}: Props) {
+  const [open, setOpen] = React.useState(false);
+  const contextValue: DrawerContextType = {
+    open,
+    setOpen,
+  };
+
+  return (
+    <Alert>
+      <AlertDialogTrigger asChild>
+        <Button variant={colorBtn}>{btnText}</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent className="z-[1000]">
+        <AlertDialogHeader>
+          <AlertDialogTitle>{btnQuestion}</AlertDialogTitle>
+          {btnSubAlert && (
+            <AlertDialogDescription>{btnSubAlert}</AlertDialogDescription>
+          )}
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Annuler</AlertDialogCancel>
+          <AlertDialogAction onClick={onClick} className="text-white">
+            Confirmer
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </Alert>
+  );
+}
