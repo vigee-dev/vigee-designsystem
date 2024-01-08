36a9e97bf7f8117227f031c2973fcd89eaa0bcdd
Vigee <contact@vigee.fr>
2024-01-08 12:28:13 +0100

    loader

--- components/Loaders/Loader.tsx
+++ components/Loaders/Loader.tsx
@@ -5,13 +5,18 @@ import Image, { StaticImageData } from "next/image";
 interface Props {
   text?: String;
   logo?: StaticImageData;
+  borderColor?: String;
 }
 
-export const Loader = ({ text, logo }: Props) => {
+export const Loader = ({ text, logo, borderColor }: Props) => {
   return (
     <div className="flex flex-col justify-center items-center gap-y-2 p-12 h-screen">
       <div className="relative flex items-center justify-center">
-        <div className="absolute h-28 w-28 border-2 border-gray-300 rounded-full animate-spin border-t-transparent" />
+        <div
+          className={`absolute h-28 w-28 border-2 ${
+            borderColor ? borderColor : "border-gray-300"
+          } rounded-full animate-spin border-t-transparent`}
+        />
 
         <Image
           src={logo ? logo : VigeeLoaderTransparent}
