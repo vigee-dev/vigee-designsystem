6554971d3b95acbdb5847e624307b5ce499b28fd
Vigee <contact@vigee.fr>
2024-01-08 15:14:41 +0100

    pagination info in utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -131,3 +131,22 @@ export const matchRolesStatus = (status: string) => {
 
   return { color, text };
 };
+
+export type PaginationInfo = {
+  current_page: number;
+  first_page_url: string;
+  from: number;
+  last_page: number;
+  last_page_url: string;
+  next_page_url: string | null;
+  path: string;
+  per_page: number;
+  prev_page_url: string | null;
+  to: number;
+  total: number;
+  links: Array<{
+    url: string | null;
+    label: string;
+    active: boolean;
+  }>;
+};
