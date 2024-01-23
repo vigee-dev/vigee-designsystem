0174bcfdb20cf1f1e410684bedb3f9704aeb7135
Vigee <contact@vigee.fr>
2024-01-23 16:41:21 +0100

    added row selection in datatable

--- components/Skeletons/Skeletons.tsx
+++ components/Skeletons/Skeletons.tsx
@@ -225,7 +225,7 @@ export function TableSkeleton() {
   return (
     <div className="mt-6 flow-root">
       <div className="inline-block min-w-full align-middle">
-        <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
+        <div className="rounded-lg bg-gray-50  md:pt-0">
           <div className="md:hidden">
             <InvoicesMobileSkeleton />
             <InvoicesMobileSkeleton />
@@ -235,26 +235,6 @@ export function TableSkeleton() {
             <InvoicesMobileSkeleton />
           </div>
           <table className="hidden min-w-full text-gray-900 md:table">
-            {/* <thead className="rounded-lg text-left text-sm font-normal">
-              <tr>
-                <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
-                  Nom
-                </th>
-                <th scope="col" className="px-3 py-5 font-medium">
-                  Téléphone
-                </th>
-                <th scope="col" className="px-3 py-5 font-medium">
-                  Email
-                </th>
-
-                <th
-                  scope="col"
-                  className="relative pb-4 pl-3 pr-6 pt-2 sm:pr-6"
-                >
-                  <span className="sr-only">Edit</span>
-                </th>
-              </tr>
-            </thead> */}
             <tbody className="bg-white">
               <TableRowSkeleton />
               <TableRowSkeleton />
