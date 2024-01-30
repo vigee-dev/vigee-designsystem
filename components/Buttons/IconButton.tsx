cccd694e009e30d06b46170f1205ac63d5418df2
Vigee <contact@vigee.fr>
2024-01-30 15:23:10 +0100

    icon button

--- components/Buttons/IconButton.tsx
+++ components/Buttons/IconButton.tsx
@@ -1,19 +1,11 @@
 "use client";
-import React, { useState, ReactElement } from "react";
-interface IconProps {
-  className?: string;
-}
+import React from "react";
 
 interface Props {
-  icon?: ReactElement<IconProps>;
-  iconHover?: ReactElement<IconProps>;
+  icon?: React.ReactNode;
 }
 
-export const IconButton = ({ icon, iconHover }: Props) => {
-  const iconClasses = "w-8 h-8 text-gray-400 ";
-  const iconHoverClasses =
-    " w-8 h-8 text-primary cursor-pointer cursor-pointer transform ease-in-out duration-200";
-
+export const IconButton = ({ icon }: Props) => {
   return (
     <div className="flex items-center justify-center  rounded-full  w-8 h-8 text-gray-400 hover:cursor-pointer hover:text-primary transform ease-in-out duration-200">
       {icon}
