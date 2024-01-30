69b25a275c5c9c78ab1cb1d5a1a48e26c19d9ef7
Vigee <contact@vigee.fr>
2024-01-30 15:17:52 +0100

    icon button

--- components/Buttons/IconButton.tsx
+++ components/Buttons/IconButton.tsx
@@ -1,21 +1,43 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, ReactElement } from "react";
+interface IconProps {
+  className?: string;
+}
 
 interface Props {
-  icon?: React.ReactNode;
-  iconHover?: React.ReactNode;
+  icon?: ReactElement<IconProps>;
+  iconHover?: ReactElement<IconProps>;
 }
 
 export const IconButton = ({ icon, iconHover }: Props) => {
   const [isHover, setIsHover] = useState(false);
 
+  const getStyledIcon = (
+    iconElement: ReactElement<IconProps> | undefined,
+    additionalClasses: string
+  ) => {
+    if (iconElement && React.isValidElement<IconProps>(iconElement)) {
+      return React.cloneElement(iconElement, {
+        className: `${iconElement.props.className || ""} ${additionalClasses}`,
+      });
+    }
+    return iconElement;
+  };
+
+  const iconClasses =
+    "text-gray-400 hover:bg-primary cursor-pointercursor-pointer";
+  const iconHoverClasses =
+    "text-primary hover:bg-primary cursor-pointercursor-pointer";
+
   return (
     <div
       onMouseEnter={() => setIsHover(true)}
       onMouseLeave={() => setIsHover(false)}
-      className="flex items-center justify-center w-8 h-8 text-gray-400 hover:text-primary hover:cursor-pointer transform ease-in-out duration-200"
+      className="flex items-center justify-center w-8 h-8 p-2 rounded-full  transform ease-in-out duration-200"
     >
-      {isHover ? iconHover : icon}
+      {isHover
+        ? getStyledIcon(iconHover, iconHoverClasses)
+        : getStyledIcon(icon, iconClasses)}
     </div>
   );
 };
