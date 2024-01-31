7352bc377eb0da90e392285a052da9642c8274f1
Vigee <contact@vigee.fr>
2024-01-31 12:57:46 +0100

    added combobox

--- components/Forms/ComboBox.tsx
+++ components/Forms/ComboBox.tsx
@@ -45,58 +45,59 @@ export function ComboBox<T extends z.ZodType<any, any, any>>({
   const [open, setOpen] = React.useState(false);
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
+    <>
       {label && (
         <FormLabel className="font-black text-primary p-0 mt-2">
           {label}
         </FormLabel>
       )}
-
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-full justify-between"
-        >
-          {value
-            ? items.find((item) => item.value === value)?.label
-            : placeholder}
-          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-full p-0">
-        <Command>
-          <CommandInput placeholder="Rechercher..." />
-          <CommandEmpty>Aucun élément trouvé.</CommandEmpty>
-          <CommandGroup>
-            {items.map((item) => (
-              <CommandItem
-                key={item.value}
-                value={item.value}
-                onSelect={(currentValue) => {
-                  const valueToUpdate =
-                    currentValue === value ? undefined : currentValue;
-                  // Assurez-vous que la mise à jour respecte le type attendu par le schéma Zod.
-                  setValue(
-                    name as Path<z.infer<T> & FieldValues>,
-                    valueToUpdate as any
-                  );
-                  setOpen(false);
-                }}
-              >
-                <Check
-                  className={cn(
-                    "mr-2 h-4 w-4",
-                    value === item.value ? "opacity-100" : "opacity-0"
-                  )}
-                />
-                {item.label}
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </Command>
-      </PopoverContent>
-    </Popover>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-full justify-between"
+          >
+            {value
+              ? items.find((item) => item.value === value)?.label
+              : placeholder}
+            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-full p-0">
+          <Command>
+            <CommandInput placeholder="Rechercher..." />
+            <CommandEmpty>Aucun élément trouvé.</CommandEmpty>
+            <CommandGroup>
+              {items.map((item) => (
+                <CommandItem
+                  key={item.value}
+                  value={item.value}
+                  onSelect={(currentValue) => {
+                    const valueToUpdate =
+                      currentValue === value ? undefined : currentValue;
+                    // Assurez-vous que la mise à jour respecte le type attendu par le schéma Zod.
+                    setValue(
+                      name as Path<z.infer<T> & FieldValues>,
+                      valueToUpdate as any
+                    );
+                    setOpen(false);
+                  }}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      value === item.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                  {item.label}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </>
   );
 }
