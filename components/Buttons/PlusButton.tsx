5e22eb3df98ba8ef81ffaaaf0527ae3a900599fd
Vigee <contact@vigee.fr>
2024-01-31 16:24:20 +0100

    newt plus button

--- components/Buttons/PlusButton.tsx
+++ components/Buttons/PlusButton.tsx
@@ -8,9 +8,9 @@ interface Props {
 export const PlusButton = ({ href }: Props) => {
   return href ? (
     <Link href={href}>
-      <PiPlusCircleSolid className="w-8 h-8 mr-2 text-primary hover:cursor-pointer transform transition-all duration-300 ease-in-out hover:scale-110" />
+      <PiPlusCircleSolid className="w-10 h-10 mr-2 text-primary hover:cursor-pointer transform transition-all duration-300 ease-in-out hover:scale-110" />
     </Link>
   ) : (
-    <PiPlusCircleSolid className="w-8 h-8 mr-2 text-primary hover:cursor-pointer transform transition-all duration-300 ease-in-out hover:scale-110" />
+    <PiPlusCircleSolid className="w-10 h-10 mr-2 text-primary hover:cursor-pointer transform transition-all duration-300 ease-in-out hover:scale-110" />
   );
 };
