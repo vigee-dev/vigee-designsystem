e2a6e6dfda4b14a635118b1390265caf3a3b19ab
Vigee <contact@vigee.fr>
2024-02-01 19:42:56 +0100

    better tooltip

--- components/Tooltip/Tooltip.tsx
+++ components/Tooltip/Tooltip.tsx
@@ -1,4 +1,10 @@
 import React from "react";
+import {
+  TooltipProvider,
+  Tooltip as TootltipShad,
+  TooltipTrigger,
+  TooltipContent,
+} from "../ui/tooltip";
 
 interface TooltipProps {
   message: string;
@@ -7,11 +13,11 @@ interface TooltipProps {
 
 export function Tooltip({ message, children }: TooltipProps) {
   return (
-    <div className="group relative flex z-50">
-      {children}
-      <span className="absolute top-10 scale-0 transition-all rounded bg-white p-2 text-xs text-primary group-hover:scale-100  shadow">
-        {message}
-      </span>
-    </div>
+    <TooltipProvider>
+      <TootltipShad>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent>{message}</TooltipContent>
+      </TootltipShad>
+    </TooltipProvider>
   );
 }
