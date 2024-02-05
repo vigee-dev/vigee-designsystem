559eac283e643a3e56110ef4f883b500c45f9ff9
Vigee <contact@vigee.fr>
2024-02-05 15:49:30 +0100

    new edit icon

--- components/Buttons/EditIcon.tsx
+++ components/Buttons/EditIcon.tsx
@@ -7,6 +7,6 @@ interface Props {
 
 export const EditIcon = ({ onClick }: Props) => {
   return (
-    <PiPencilEditBoxStroke className="text-text-primary-light hover:text-primary hover:cursor-pointer transform transition-ease-in-out duration-300 ease-in-out" />
+    <PiPencilEditBoxStroke className="text-primary-light hover:text-primary hover:cursor-pointer transform transition-ease-in-out duration-300 ease-in-out" />
   );
 };
