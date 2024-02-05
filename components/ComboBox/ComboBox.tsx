a7e64433ad98da740837e6c75d9c3d85d23f1d7d
Vigee <contact@vigee.fr>
2024-02-05 11:19:20 +0100

    flex col for combo box

--- components/ComboBox/ComboBox.tsx
+++ components/ComboBox/ComboBox.tsx
@@ -51,7 +51,7 @@ export function ComboBox({
             variant="outline"
             role="combobox"
             aria-expanded={open}
-            className="w-full justify-between bg-input border-0 gap-x-2"
+            className="w-full justify-between bg-input border-0 gap-x-2 items-center"
           >
             {icon && icon}
             {value
