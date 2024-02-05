79d47cc36a7d0b37292ed5c6e821885c0376f723
Vigee <contact@vigee.fr>
2024-02-05 15:44:53 +0100

    new edit icon

---
+++ components/Buttons/EditIcon.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { PiPencilEditBoxStroke } from "../../icons/PikaIcons";
+
+export const EditIcon = () => {
+  return (
+    <PiPencilEditBoxStroke className="text-text-primaryLight hover:text-primary hover:cursor-pointer transform transition-ease-in-out duration-300 ease-in-out" />
+  );
+};
