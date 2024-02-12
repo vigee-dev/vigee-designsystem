020b4ad8f49a7a09c74fd032df7f6ca60717e0a7
Vigee <contact@vigee.fr>
2024-02-12 14:20:20 +0100

    reset password page

--- components/Login/ResetPassword.tsx
+++ components/Login/ResetPassword.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { toast } from "react-hot-toast";
+import { toast } from "sonner";
 import { useResetPassword } from "@/app/api/password/usePassword";
 
 type ResetPasswordProps = {
