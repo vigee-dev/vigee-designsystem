30a612b643185b784361ab56a1dff9dba02e5d1d
Vigee <contact@vigee.fr>
2024-02-13 18:58:03 +0100

    fix ägeHeader padding

--- components/Buttons/PlusButton.tsx
+++ components/Buttons/PlusButton.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { PiPlusSquareDuoStroke } from "../../icons/PikaIcons";
-import { Tooltip } from "../Tooltip/Tooltip";
+import Tooltip from "@/app/components/Tooltip/Tooltip";
 
 interface Props {
   href?: string;
@@ -9,11 +9,11 @@ interface Props {
 
 export const PlusButton = ({ href, tooltip }: Props) => {
   return href ? (
-    <Link href={href}>
-      <Tooltip message={tooltip ?? "Ajouter"}>
+    <Tooltip message={tooltip ?? "Ajouter"}>
+      <Link href={href}>
         <PiPlusSquareDuoStroke className="w-10 h-10 mr-2 text-primary hover:cursor-pointer transform transition-all duration-300 ease-in-out hover:scale-110" />
-      </Tooltip>
-    </Link>
+      </Link>
+    </Tooltip>
   ) : (
     <Tooltip message={tooltip ?? "Ajouter"}>
       <PiPlusSquareDuoStroke className="w-10 h-10 mr-2 text-primary hover:cursor-pointer transform transition-all duration-300 ease-in-out hover:scale-110" />
