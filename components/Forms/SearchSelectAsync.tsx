d949d88c91ade033aee5dcf4cf8cbb1c732baf64
Minster Robin <minster.robin.pro@gmail.com>
2024-02-15 17:05:14 +0100

    ADDED: Added wip of react-select SearchSelectAsync.tsx

---
+++ components/Forms/SearchSelectAsync.tsx
@@ -0,0 +1,67 @@
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/app/components/vigee-designsystem/components/ui/form";
+import SearchAndSelectAsync from "react-select/async";
+import React from "react";
+import { UseFormReturn, FieldValues, Path } from "react-hook-form";
+import {z} from "zod";
+import {GroupBase, MultiValue, SingleValue} from "react-select";
+
+interface SearchSelectAsyncInterface<Option, T extends z.ZodType<any, any>> {
+  name: Path<z.infer<T> & FieldValues>,
+  form?: UseFormReturn<z.infer<T> & FieldValues>,
+  disabled?: boolean
+  placeholder?: string
+  loadOptions: (query: string) => (Promise<Option[]>)
+  isClearable?: boolean
+  preprocessOnChange?: (e: SingleValue<Option>) => any
+  defaultOptions?: Option[]
+}
+
+// TODO Better way to handle isMulti, Option type etc ...
+export default function SearchSelectAsync<Option, T extends z.ZodType<any, any, any>>({
+  name,
+  form,
+  placeholder = "Rechercher...",
+  loadOptions,
+  disabled = false,
+  isClearable = true,
+  preprocessOnChange,
+  defaultOptions,
+}: SearchSelectAsyncInterface<Option, T>) {
+  return (<FormField
+    control={form?.control}
+    name={name}
+    render={({ field }) => (
+      <FormItem>
+        <FormLabel className="font-black text-primary">Second commercial</FormLabel>
+        <FormControl>
+          {/* TODO Debounce loadoptions ? */}
+          {/* TODO typeof isMulti ?*/}
+          <SearchAndSelectAsync<Option, false, GroupBase<Option>>
+            isClearable={isClearable}
+            placeholder={placeholder}
+            isDisabled={disabled} //TODO pass disabled from field.disabled ?
+            onChange={(e) => {
+              if(preprocessOnChange) {
+                const value = preprocessOnChange(e)
+                return field.onChange(value)
+              }
+              else return field.onChange(e)
+            }}
+            onBlur={field.onBlur}
+            defaultOptions={defaultOptions || []}
+            loadOptions={loadOptions}
+            ref={field.ref}
+          />
+
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    )}
+  />)
+}
