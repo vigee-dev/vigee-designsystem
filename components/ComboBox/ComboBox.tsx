8f0705d4e8cd92a18875087d1a00cdfda72c0513
Vigee <contact@vigee.fr>
2024-02-16 11:30:30 +0100

    scroll area combo box

--- components/ComboBox/ComboBox.tsx
+++ components/ComboBox/ComboBox.tsx
@@ -64,41 +64,40 @@ export function ComboBox({
           >
             {icon && icon}
             {value
-              ? filteredItems.find(item => item.value === value)?.label
+              ? items.find(item => item.value === value)?.label
               : placeholder}
             <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
           </Button>
         </PopoverTrigger>
         <PopoverContent className="w-full p-0">
           <Command shouldFilter={false}>
-            <CommandInput
-              value={searchText}
-              placeholder="Rechercher..."
-              autoFocus
-              onValueChange={text => setSearchText(text)}
-            />
+            <CommandInput placeholder="Rechercher..." autoFocus />
             <CommandEmpty>Aucun élément trouvé.</CommandEmpty>
             <CommandGroup className="max-h-[200px]">
               <ScrollArea className="h-[200px]">
                 {filteredItems.length > 0 &&
-                  filteredItems.map(item => (
-                    <CommandItem
-                      key={item.value}
-                      value={item.value}
-                      onSelect={value => {
-                        onChange(item.value === value ? undefined : item.value);
-                        setOpen(false);
-                      }}
-                    >
-                      <Check
-                        className={cn(
-                          "mr-2 h-4 w-4",
-                          value === item.value ? "opacity-100" : "opacity-0"
-                        )}
-                      />
-                      {item.label}
-                    </CommandItem>
-                  ))}
+                  filteredItems.map(item => {
+                    return (
+                      <CommandItem
+                        key={item.value}
+                        value={item.value}
+                        onSelect={() => {
+                          onChange(
+                            item.value === value ? undefined : item.value
+                          );
+                          setOpen(false);
+                        }}
+                      >
+                        <Check
+                          className={cn(
+                            "mr-2 h-4 w-4",
+                            value === item.value ? "opacity-100" : "opacity-0"
+                          )}
+                        />
+                        {item.label}
+                      </CommandItem>
+                    );
+                  })}
               </ScrollArea>
             </CommandGroup>
           </Command>
