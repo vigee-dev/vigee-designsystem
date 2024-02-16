ad1ef8721206b6fae83d59ee3e4e6cb16c3f446a
Vigee <contact@vigee.fr>
2024-02-16 11:28:30 +0100

    scroll area combo box

--- components/ComboBox/ComboBox.tsx
+++ components/ComboBox/ComboBox.tsx
@@ -82,7 +82,14 @@ export function ComboBox({
               <ScrollArea className="h-[200px]">
                 {filteredItems.length > 0 &&
                   filteredItems.map(item => (
-                    <CommandItem key={item.value} value={item.value}>
+                    <CommandItem
+                      key={item.value}
+                      value={item.value}
+                      onSelect={value => {
+                        onChange(item.value === value ? undefined : item.value);
+                        setOpen(false);
+                      }}
+                    >
                       <Check
                         className={cn(
                           "mr-2 h-4 w-4",
