e6623b330185b1573048e0e853523ba6474e073b
Vigee <contact@vigee.fr>
2024-02-22 15:01:17 +0100

    error illustration

--- components/Forms/SelectSearch.tsx
+++ components/Forms/SelectSearch.tsx
@@ -7,12 +7,12 @@ import {
 } from "../../components/ui/form";
 import SelectAndSearch from "react-select";
 import { UseFormReturn, FieldValues, Path } from "react-hook-form";
-import {GroupBase, OnChangeValue} from "react-select";
+import { GroupBase, OnChangeValue } from "react-select";
 
 interface SearchSelectInterface<
   T extends FieldValues,
   Option,
-  IsMulti extends boolean = false,
+  IsMulti extends boolean = false
 > {
   name: Path<T>;
   form: UseFormReturn<T>;
@@ -22,20 +22,24 @@ interface SearchSelectInterface<
   preprocessOnChange?: (e: OnChangeValue<Option, IsMulti>) => any;
   options: Option[];
   label?: string;
-  isMulti?: IsMulti
+  isMulti?: IsMulti;
 }
 
 // TODO Better way to handle isMulti, Option type etc ...
-export default function SelectSearch<T extends FieldValues, Option, IsMulti extends boolean = false>({
- name,
- label,
- form,
- placeholder = "Rechercher...",
- disabled = false,
- isClearable = true,
- preprocessOnChange,
- options,
- isMulti
+export default function SelectSearch<
+  T extends FieldValues,
+  Option,
+  IsMulti extends boolean = false
+>({
+  name,
+  label,
+  form,
+  placeholder = "Rechercher...",
+  disabled = false,
+  isClearable = true,
+  preprocessOnChange,
+  options,
+  isMulti,
 }: SearchSelectInterface<T, Option, IsMulti>) {
   return (
     <FormField
@@ -43,7 +47,9 @@ export default function SelectSearch<T extends FieldValues, Option, IsMulti exte
       name={name}
       render={({ field }) => (
         <FormItem>
-          {label && (<FormLabel className="font-black text-primary">{label}</FormLabel>)}
+          {label && (
+            <FormLabel className="font-black text-primary">{label}</FormLabel>
+          )}
           <FormControl>
             {/* TODO Debounce loadoptions ? */}
             {/* TODO typeof isMulti ?*/}
@@ -61,6 +67,7 @@ export default function SelectSearch<T extends FieldValues, Option, IsMulti exte
               options={options}
               ref={field.ref}
               isMulti={isMulti}
+              className="border-none"
             />
           </FormControl>
           <FormMessage />
