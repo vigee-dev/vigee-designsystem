f98b8dffb7e06a78f3e1975bd1387edfa6876529
Vigee <contact@vigee.fr>
2024-02-22 14:52:48 +0100

    error illustration

--- components/Illustration/Error.tsx
+++ components/Illustration/Error.tsx
@@ -23,7 +23,7 @@ export default function ErrorIllustration({
         <Image
           width={400}
           height={400}
-          className="mx-auto h-32 w-auto"
+          className="mx-auto h-36 w-auto"
           src={error}
           alt="Erreur"
         />
