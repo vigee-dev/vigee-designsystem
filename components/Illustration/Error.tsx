0dbe26922ccff8c3432997cb0b8396a44abbd550
Vigee <contact@vigee.fr>
2024-02-22 14:49:50 +0100

    error illustration

--- components/Illustration/Error.tsx
+++ components/Illustration/Error.tsx
@@ -16,31 +16,30 @@ export default function ErrorIllustration({
   children,
 }: IllustrationProps) {
   return (
-    <div className="flex flex-col items-center justify-center bg-primary h-screen w-full px-12 ">
+    <div className="flex flex-col items-center justify-center  w-full px-12 ">
       {error && (
         <Image
-          width={200}
-          height={200}
-          className="mx-auto h-96 w-auto"
+          width={400}
+          height={400}
+          className="mx-auto h-32 w-auto"
           src={error}
           alt="Erreur"
         />
       )}
       <h1
         className={
-          "text-5xl text-gray-300 font-black text-center pt-6 font-display"
+          "text-xl text-gray-300 font-black text-center pt-6 font-display"
         }
       >
         Une erreur s'est produite.
       </h1>
-      {subtitle && (
-        <Link
-          className="text-xl text-slate-500   text-center font-display"
-          href={"mailto:support@vigee.fr"}
-        >
-          Contacter le support
-        </Link>
-      )}
+
+      <Link
+        className="text-xl text-slate-500   text-center font-display"
+        href={"mailto:support@vigee.fr"}
+      >
+        Contacter le support
+      </Link>
 
       {children}
     </div>
