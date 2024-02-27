07fb96971b46b078cdc4aff65a889f09ef633f47
Vigee <contact@vigee.fr>
2024-02-27 14:17:48 +0100

    delete button

--- components/Buttons/DeleteButton.tsx
+++ components/Buttons/DeleteButton.tsx
@@ -6,11 +6,17 @@ import { Button } from "./Button";
 interface Props {
   onClick?: () => void;
   className?: string;
+  type?: "button" | "submit" | "reset";
 }
 
-export const DeleteButton = ({ onClick, className }: Props) => {
+export const DeleteButton = ({
+  onClick,
+  className,
+  type = "button",
+}: Props) => {
   return (
     <Button
+      type={type}
       variant="outline"
       className={cn(
         "flex text-gray-600 gap-x-4 w-full hover:bg-red-500 hover:text-white transformation ease-in-out duration-300",
