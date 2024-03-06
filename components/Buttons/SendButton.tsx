17322e6a75d98fe3e2a335378e96c01883e45af4
Vigee <contact@vigee.fr>
2024-03-06 16:17:26 +0100

    send button

---
+++ components/Buttons/SendButton.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { PiEye02OnDuoStroke } from "../../icons/PikaIcons";
+import { cn } from "../../lib/utils";
+import Link from "next/link";
+import { Tooltip } from "../Tooltip/Tooltip";
+
+interface Props {
+  onClick?: () => void;
+  className?: string;
+  href?: string;
+  tooltip?: string;
+  text?: string;
+}
+
+export const SendButton = ({
+  onClick,
+  className,
+  href,
+  tooltip = "Voir",
+  text,
+}: Props) => {
+  return href ? (
+    <Tooltip message={tooltip}>
+      <Link href={href} className="flex items-center gap-x-2">
+        {text}{" "}
+        <PiEye02OnDuoStroke
+          className={cn(
+            "text-gray-400 hover:text-primary hover:cursor-pointer transform transition-ease-in-out duration-300 ease-in-out w-5 h-5",
+            className
+          )}
+        />
+      </Link>
+    </Tooltip>
+  ) : (
+    <Tooltip message={tooltip}>
+      <div className="flex items-center gap-x-2">
+        {text}
+        <PiEye02OnDuoStroke
+          className={cn(
+            "text-gray-400 hover:text-primary hover:cursor-pointer transform transition-ease-in-out duration-300 ease-in-out w-5 h-5",
+            className
+          )}
+          onClick={onClick}
+        />
+      </div>
+    </Tooltip>
+  );
+};
