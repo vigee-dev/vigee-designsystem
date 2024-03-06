77f938886795e3e2238abf45eb1dff797dc4fe62
Vigee <contact@vigee.fr>
2024-03-06 11:04:22 +0100

    download button

--- components/Buttons/EditButton.tsx
+++ components/Buttons/EditButton.tsx
@@ -1,34 +1,42 @@
 import React from "react";
-import {
-  PiPencilEditBoxDuoSolid,
-  PiPencilEditBoxStroke,
-} from "../../icons/PikaIcons";
+import { PiPencilEditBoxDuoSolid } from "../../icons/PikaIcons";
 import { cn } from "../../lib/utils";
 import Link from "next/link";
+import { Tooltip } from "../Tooltip/Tooltip";
 
 interface Props {
   onClick?: () => void;
   className?: string;
   href?: string;
+  tooltip?: string;
 }
 
-export const EditButton = ({ onClick, className, href }: Props) => {
+export const EditButton = ({
+  onClick,
+  className,
+  href,
+  tooltip = "Modifier",
+}: Props) => {
   return href ? (
-    <Link href={href}>
+    <Tooltip message={tooltip}>
+      <Link href={href}>
+        <PiPencilEditBoxDuoSolid
+          className={cn(
+            " hover:text-primary hover:cursor-pointer transform transition-ease-in-out duration-300 ease-in-out w-5 h-5",
+            className
+          )}
+        />
+      </Link>
+    </Tooltip>
+  ) : (
+    <Tooltip message={tooltip}>
       <PiPencilEditBoxDuoSolid
         className={cn(
           " hover:text-primary hover:cursor-pointer transform transition-ease-in-out duration-300 ease-in-out w-5 h-5",
           className
         )}
+        onClick={onClick}
       />
-    </Link>
-  ) : (
-    <PiPencilEditBoxDuoSolid
-      className={cn(
-        " hover:text-primary hover:cursor-pointer transform transition-ease-in-out duration-300 ease-in-out w-5 h-5",
-        className
-      )}
-      onClick={onClick}
-    />
+    </Tooltip>
   );
 };
