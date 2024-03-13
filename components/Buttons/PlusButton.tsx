f8528f243e35dd8912e05cdcfa55eec249f923af
Vigee <contact@vigee.fr>
2024-03-13 10:10:03 +0100

    style plus button

--- components/Buttons/PlusButton.tsx
+++ components/Buttons/PlusButton.tsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
-import { PiPlusSquareDuoStroke } from "../../icons/PikaIcons";
+import {
+  PiPlusSquareContrast,
+  PiPlusSquareDuoSolid,
+  PiPlusSquareDuoStroke,
+} from "../../icons/PikaIcons";
 import { Tooltip } from "../Tooltip/Tooltip";
 
 interface Props {
@@ -12,18 +16,18 @@ export const PlusButton = ({ href, onClick, tooltip }: Props) => {
   return href ? (
     <Tooltip message={tooltip ?? "Ajouter"}>
       <Link href={href}>
-        <PiPlusSquareDuoStroke className="w-10 h-10 mr-2 text-primary hover:cursor-pointer transform transition-all duration-300 ease-in-out hover:scale-110" />
+        <PiPlusSquareDuoSolid className="w-10 h-10 mr-2 text-primary-light hover:cursor-pointer transform transition-all duration-300 ease-in-out hover:scale-110 hover:text-primary" />
       </Link>
     </Tooltip>
   ) : onClick ? (
     <div onClick={onClick}>
       <Tooltip message={tooltip ?? "Ajouter"}>
-        <PiPlusSquareDuoStroke className="w-10 h-10 mr-2 text-primary hover:cursor-pointer transform transition-all duration-300 ease-in-out hover:scale-110" />
+        <PiPlusSquareDuoSolid className="w-10 h-10 mr-2 text-primary-light hover:cursor-pointer transform transition-all duration-300 ease-in-out hover:scale-110 hover:text-primary" />
       </Tooltip>
     </div>
   ) : (
     <Tooltip message={tooltip ?? "Ajouter"}>
-      <PiPlusSquareDuoStroke className="w-10 h-10 mr-2 text-primary hover:cursor-pointer transform transition-all duration-300 ease-in-out hover:scale-110" />
+      <PiPlusSquareDuoSolid className="w-10 h-10 mr-2 text-primary-light hover:cursor-pointer transform transition-all duration-300 ease-in-out hover:scale-110 hover:text-primary" />
     </Tooltip>
   );
 };
