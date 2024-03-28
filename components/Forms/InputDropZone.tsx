1c4a7eb6f5af762abbf586bef221b0476f88c792
Vigee <contact@vigee.fr>
2024-03-28 16:28:53 +0100

    input drop zone

--- components/Forms/InputDropZone.tsx
+++ components/Forms/InputDropZone.tsx
@@ -1,10 +1,19 @@
-'use client'
+"use client";
 
 import React from "react";
 import { UseFormReturn, FieldValues, Path } from "react-hook-form";
 import { useRef, useState } from "react";
-import {DrawerMobile} from "@/app/components/vigee-designsystem/components/Forms/DrawerMobile";
-import {EyeIcon} from "@heroicons/react/24/outline";
+import { DrawerMobile } from "@/app/components/vigee-designsystem/components/Forms/DrawerMobile";
+import { EyeIcon } from "@heroicons/react/24/outline";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselPrevious,
+  CarouselNext,
+} from "../ui/carousel";
+import { Card, CardContent } from "../ui/card";
+import { PiDeleteDustbin01DuoSolid } from "../../icons/PikaIcons";
 
 type Props<T extends FieldValues> = {
   form?: UseFormReturn<T>;
@@ -14,7 +23,12 @@ type Props<T extends FieldValues> = {
 };
 
 // TODO styles @vigee
-export default function InputDropZoneFile<T extends FieldValues>({form, name, extensions, multiple = false}: Props<T>) {
+export default function InputDropZoneFile<T extends FieldValues>({
+  form,
+  name,
+  extensions,
+  multiple = false,
+}: Props<T>) {
   function getPossibleExtensions(strings: string[] | undefined): string {
     // Déterminer les extensions possibles
     const extensions = strings || [];
@@ -30,34 +44,36 @@ export default function InputDropZoneFile<T extends FieldValues>({form, name, ex
 
   const [dragActive, setDragActive] = useState<boolean>(false);
   const inputRef = useRef<any>(null);
-  const [files, setFiles] = useState<(File | {id: number, signedUrl: string, filename: string})[]>(form?.getValues(name) ? form?.getValues(name) : []);
+  const [files, setFiles] = useState<
+    (File | { id: number; signedUrl: string; filename: string })[]
+  >(form?.getValues(name) ? form?.getValues(name) : []);
 
   const handleFiles = (newFiles: File[]) => {
     if (multiple) {
-      const updatedFiles = [...files, ...newFiles]
-      setFiles(updatedFiles)
+      const updatedFiles = [...files, ...newFiles];
+      setFiles(updatedFiles);
       // @ts-ignore TOIMPROVE remove ts-ignore and find a way to fix the type error
-      if (form) form.setValue(name, updatedFiles)
+      if (form) form.setValue(name, updatedFiles);
     } else {
-      setFiles(Array.from(newFiles))
+      setFiles(Array.from(newFiles));
       // @ts-ignore TOIMPROVE remove ts-ignore and find a way to fix the type error
-      if (form) form.setValue(name, Array.from(newFiles))
+      if (form) form.setValue(name, Array.from(newFiles));
     }
-  }
+  };
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     e.preventDefault();
 
-    const files = e.target.files
-    if (files) handleFiles(Array.from(files))
+    const files = e.target.files;
+    if (files) handleFiles(Array.from(files));
   }
 
   function handleDrop(e: React.DragEvent<HTMLDivElement>) {
     e.preventDefault();
     e.stopPropagation();
 
-    const files = e.dataTransfer.files
-    if (files) handleFiles(Array.from(files))
+    const files = e.dataTransfer.files;
+    if (files) handleFiles(Array.from(files));
   }
 
   function handleDragLeave(e: any) {
@@ -79,15 +95,15 @@ export default function InputDropZoneFile<T extends FieldValues>({form, name, ex
   }
 
   function handleRemoveFile(e: React.MouseEvent, id: any) {
-    e.preventDefault()
-    e.stopPropagation()
+    e.preventDefault();
+    e.stopPropagation();
 
-    const updatedFiles = files
+    const updatedFiles = files;
     updatedFiles.splice(id, 1);
     setFiles(updatedFiles);
 
     // @ts-ignore TOIMPROVE remove ts-ignore and find a way to fix the type error
-    if (form) form.setValue(name, updatedFiles)
+    if (form) form.setValue(name, updatedFiles);
   }
 
   function openFileExplorer() {
@@ -100,7 +116,7 @@ export default function InputDropZoneFile<T extends FieldValues>({form, name, ex
       <div
         className={`${
           dragActive ? "bg-gray-200" : "bg-gray-100"
-        } w-full  flex flex-col p-8 items-center justify-center  h-48 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600`}
+        } w-full flex gap-4 p-8 items-center justify-center h-fit border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 overflow-hidden`}
         onDragEnter={handleDragEnter}
         onSubmit={e => e.preventDefault()}
         onDrop={handleDrop}
@@ -119,40 +135,50 @@ export default function InputDropZoneFile<T extends FieldValues>({form, name, ex
           accept=".xlsx,.xls,image/*,.doc, .docx,.ppt, .pptx,.txt,.pdf"
         />
 
-        {!(files.length > 0 ) ? (
-          <svg
-            className="w-8 h-8 mb-4 text-gray-400 dark:text-gray-400"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 16"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-            />
-          </svg>
-        ) : (files.map((file, index) => <DrawerMobile // TODO icon display instead of image for images files, remove the drawer too (or add pdf preview for PDF) @vigee
-          key={index}
-          title={'Preview'}
-          description={''}
-          icon={<EyeIcon width={32} height={32} className={"text-gray-400"}/>}
-          trigger={<img
-            key={index}
-            src={file instanceof File ? URL.createObjectURL(file) : file.signedUrl}
-            alt="Aperçu du fichier"
-            className="h-32 w-32 rounded-lg"
-          />}>
-            <img
-              src={file instanceof File ? URL.createObjectURL(file) : file.signedUrl}
-              alt="Aperçu du fichier"
-              className="h-full w-fit rounded-lg"
-            />
-        </DrawerMobile>
-        ))}
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4  items-center ">
+          {!(files.length > 0) ? (
+            <svg
+              className="w-8 h-8 mb-4 text-gray-400 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 16"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+              />
+            </svg>
+          ) : (
+            files.map((file, index) => (
+              <div key={index}>
+                <Card className="rounded-xl w-36 h-36 p-2 ">
+                  <span
+                    className="text-red-500 cursor-pointer "
+                    onClick={e => handleRemoveFile(e, index)}
+                  >
+                    <PiDeleteDustbin01DuoSolid />
+                  </span>
+                  <CardContent className="flex aspect-square items-center justify-center ">
+                    <img
+                      key={index}
+                      src={
+                        file instanceof File
+                          ? URL.createObjectURL(file)
+                          : file.signedUrl
+                      }
+                      alt="Aperçu du fichier"
+                      className=" rounded-lg"
+                    />
+                  </CardContent>
+                </Card>
+              </div>
+            ))
+          )}
+        </div>
 
         {files?.length === 0 && (
           <p>
@@ -163,15 +189,6 @@ export default function InputDropZoneFile<T extends FieldValues>({form, name, ex
             pour envoyer
           </p>
         )}
-
-        <div className="flex flex-col items-center p-3">
-          {files.map((file, index) => (
-            <div key={index} className="flex flex-row space-x-5">
-              <span>{file instanceof File ? file.name : file.filename}</span>
-              <span className="text-red-500 cursor-pointer" onClick={(e) => handleRemoveFile(e, index)}>supprimer</span>
-            </div>
-          ))}
-        </div>
       </div>
     </div>
   );
