6ab7b0589f6cffe80ed51136ab381a9ef23a2bc4
Julien <35800390+JulienEmrot@users.noreply.github.com>
2024-03-28 18:17:29 +0100

    Add OnBlur TextArea

--- components/Forms/TextArea.tsx
+++ components/Forms/TextArea.tsx
@@ -39,70 +39,71 @@ type Props<T extends FieldValues> = {
 };
 
 export default function TextArea<T extends FieldValues>({
-  form,
-  id,
-  name,
-  required,
-  label,
-  placeholder,
-  count,
-  max,
-  minHeight = "32",
-  defaultValue,
-  onBlur,
-  onChange,
-  className,
-  descr,
-  disabled,
-  helpComponent,
-}: Props<T>) {
+                                                          form,
+                                                          id,
+                                                          name,
+                                                          required,
+                                                          label,
+                                                          placeholder,
+                                                          count,
+                                                          max,
+                                                          minHeight = "32",
+                                                          defaultValue,
+                                                          onBlur,
+                                                          onChange,
+                                                          className,
+                                                          descr,
+                                                          disabled,
+                                                          helpComponent,
+                                                        }: Props<T>) {
   const [charCount, setCharCount] = useState(0); // État local pour le compteur de caractères
 
   return (
-    <FormField
-      control={form?.control}
-      name={name}
-      rules={{ required }}
-      render={({ field }) => (
-        <FormItem className={className}>
-          <HoverCard>
-            <div className="flex items-center justify-between ">
-              {label && (
-                <FormLabel className="font-black text-primary mt-2">
-                  {label}
-                </FormLabel>
-              )}
-              {helpComponent && (
-                <HoverCardTrigger>
-                  <PiQuestionMarkCircleDuoStroke className="w-5 h-5 hover:text-primary hover:cursor-pointer text-gray-400" />
-                </HoverCardTrigger>
-              )}
-            </div>
+      <FormField
+          control={form?.control}
+          name={name}
+          rules={{ required }}
+          render={({ field }) => (
+              <FormItem className={className}>
+                <HoverCard>
+                  <div className="flex items-center justify-between ">
+                    {label && (
+                        <FormLabel className="font-black text-primary mt-2">
+                          {label}
+                        </FormLabel>
+                    )}
+                    {helpComponent && (
+                        <HoverCardTrigger>
+                          <PiQuestionMarkCircleDuoStroke className="w-5 h-5 hover:text-primary hover:cursor-pointer text-gray-400" />
+                        </HoverCardTrigger>
+                    )}
+                  </div>
 
-            {helpComponent && (
-              <HoverCardContent>
-                <div className="p-2">{helpComponent}</div>
-              </HoverCardContent>
-            )}
-          </HoverCard>
-          <Textarea
-            {...field}
-            placeholder={placeholder ?? ""}
-            onChange={e => {
-              if (count) {
-                setCharCount(e.target.value.length); // Mise à jour du compteur de caractères
-              }
-              field.onChange(e);
-            }}
-            className={`min-h-${minHeight || "16"} h-${
-              minHeight || "10"
-            } resize-none font-medium bg-input border-none`}
-            disabled={disabled}
-          />
-          {descr && <FormDescription>{descr}</FormDescription>}
-          <FormMessage />
-        </FormItem>
-      )}
-    />
+                  {helpComponent && (
+                      <HoverCardContent>
+                        <div className="p-2">{helpComponent}</div>
+                      </HoverCardContent>
+                  )}
+                </HoverCard>
+                <Textarea
+                    {...field}
+                    placeholder={placeholder ?? ""}
+                    onBlur={onBlur}
+                    onChange={(e) => {
+                      if (count) {
+                        setCharCount(e.target.value.length); // Mise à jour du compteur de caractères
+                      }
+                      field.onChange(e);
+                    }}
+                    className={`min-h-${minHeight || "16"} h-${
+                        minHeight || "10"
+                    } resize-none font-medium bg-input border-none`}
+                    disabled={disabled}
+                />
+                {descr && <FormDescription>{descr}</FormDescription>}
+                <FormMessage />
+              </FormItem>
+          )}
+      />
   );
 }
