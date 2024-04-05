cd3aa337710cd980b61d38dab9785e5e743b1430
Vigee <contact@vigee.fr>
2024-04-05 22:45:46 +0200

    slider fix

--- components/Slider/Slider.tsx
+++ components/Slider/Slider.tsx
@@ -6,20 +6,25 @@ type SliderProps = React.ComponentProps<typeof ShadSlider>;
 
 export function Slider({ className, ...props }: SliderProps) {
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex justify-between w-[60%]">
+    <div className="flex flex-col  w-full">
+      <div className="flex justify-between  gap-x-2 ">
         <p className="text-gray-400 text-md">{props.title}</p>
-        <TypographyH2 className="text-primary font-bold">
+        <TypographyH2 className="text-primary font-bold ">
           {props.value}%
         </TypographyH2>
       </div>
-      <ShadSlider
-        defaultValue={props.defaultValue}
-        max={props.max}
-        step={props.step}
-        className={cn("w-[60%] text-gray-400 ", className)}
-        {...props}
-      />
+
+      <div className="flex gap-x-2 w-full">
+        <p className="text-gray-400">{props.min}</p>
+        <ShadSlider
+          defaultValue={props.defaultValue}
+          max={props.max}
+          step={props.step}
+          className={cn("w-full text-gray-400 ", className)}
+          {...props}
+        />
+        <p className="text-gray-400">{props.max}</p>
+      </div>
     </div>
   );
 }
