6ebd949e31f248408b88ff98bb65d4c830725d51
Vigee <contact@vigee.fr>
2024-04-08 18:53:27 +0200

    upload button

--- components/Buttons/UploadButton.tsx
+++ components/Buttons/UploadButton.tsx
@@ -1,45 +1,26 @@
 import React from "react";
 import { Button } from "./Button";
-import {
-  PiCloudArrowDownloadStroke,
-  PiDownloadDownDuoSolid,
-  PiUploadUpDuoSolid,
-} from "../../icons/PikaIcons";
+import { PiUploadUpDuoSolid } from "../../icons/PikaIcons";
 import { cn } from "../../lib/utils";
 
 interface DownloadButtonProps {
   onClick?: () => void;
-  noText?: boolean;
   className?: string;
+  text?: string;
 }
 
 export const UploadButton = ({
   onClick,
-  noText,
   className,
+  text,
 }: DownloadButtonProps) => {
-  return noText ? (
-    <Button
-      onClick={onClick}
-      variant="outline"
-      className="bg-transparent border-none p-0"
-    >
-      <PiUploadUpDuoSolid
-        className={cn(
-          " text-gray-500 mx-2 hover:text-primary hover:cursor-pointer transform transition-ease-in-out",
-          className
-        )}
-      />
-    </Button>
-  ) : (
-    <Button onClick={onClick} variant="outline" className="text-gray-600">
-      Envoyer
-      <PiUploadUpDuoSolid
-        className={cn(
-          " text-gray-500 mx-2 hover:text-primary hover:cursor-pointer transform transition-ease-in-out",
-          className
-        )}
-      />
-    </Button>
-  );
+  <Button onClick={onClick} variant="outline" className="text-gray-600">
+    {text}
+    <PiUploadUpDuoSolid
+      className={cn(
+        " text-gray-500 mx-2 hover:text-primary hover:cursor-pointer transform transition-ease-in-out",
+        className
+      )}
+    />
+  </Button>;
 };
