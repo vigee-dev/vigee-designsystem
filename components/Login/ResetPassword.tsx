b82ce6578948a916d434cc376cf9e93c993f94fb
Julien <35800390+JulienEmrot@users.noreply.github.com>
2024-04-09 04:36:04 +0200

    mutation tanstack old

--- components/Login/ResetPassword.tsx
+++ components/Login/ResetPassword.tsx
@@ -100,7 +100,7 @@ export default function ResetPassword({ token }: ResetPasswordProps) {
         <div>
           <button
             type="submit"
-            disabled={mutation.isPending}
+            disabled={isPending}
             className="flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
           >
             Réinitialiser mon mot de passe
