abc3b55a2cc112a7ac09a8d3c3450b89c7671258
Vigee <contact@vigee.fr>
2024-04-17 19:37:18 +0200

    new Button component

--- components/Buttons/Button.tsx
+++ components/Buttons/Button.tsx
@@ -1,18 +1,23 @@
-"use client";
-import React from "react";
+import React, { useMemo } from "react";
 import Link from "next/link";
 import { Button as ShadButton } from "../ui/button";
 import { LoadingButton } from "./LoadingButton";
 import { cn } from "../../lib/utils";
-
+import {
+  PiSendPlaneHorizontalContrast,
+  PiEye02OnContrast,
+  PiPlusSquareDuoSolid,
+  PiDownloadDownDuoSolid,
+  PiDeleteDustbin01DuoStroke,
+  PiUploadUpDuoSolid,
+  PiArrowLeftStroke,
+} from "../../icons/PikaIcons";
+import { Tooltip } from "../Tooltip/Tooltip";
 interface ButtonProps {
-  children: React.ReactNode;
+  children?: React.ReactNode;
   onClick?: () => void;
-  reversed?: boolean;
-  type?: "button" | "submit" | "reset" | undefined;
+  type?: "button" | "submit" | "reset";
   disabled?: boolean;
-  size?: "sm" | "md" | "lg";
-  ring?: string;
   href?: string;
   className?: string;
   pending?: boolean;
@@ -22,45 +27,128 @@ interface ButtonProps {
     | "outline"
     | "secondary"
     | "ghost"
-    | "link"
-    | null
-    | undefined;
+    | "link";
+  icon?: keyof typeof iconMap;
+  iconComponent?: React.ComponentType<{ className?: string }>;
+  tooltip?: string;
 }
 
-export function Button({
+const iconMap = {
+  send: PiSendPlaneHorizontalContrast,
+  view: PiEye02OnContrast,
+  add: PiPlusSquareDuoSolid,
+  download: PiDownloadDownDuoSolid,
+  trash: PiDeleteDustbin01DuoStroke,
+  upload: PiUploadUpDuoSolid,
+  back: PiArrowLeftStroke,
+};
+
+export const Button = React.memo<ButtonProps>(
+  ({
+    children,
+    onClick,
+    variant,
+    type = "button",
+    disabled,
+    href,
+    className,
+    pending,
+    icon,
+    iconComponent,
+    tooltip,
+  }) => {
+    return pending ? (
+      <LoadingButton
+        variant={variant}
+        className={cn("font-bold text-sm border", className)}
+      >
+        {children}
+      </LoadingButton>
+    ) : href ? (
+      <Link href={href}>
+        <ButtonComponent
+          variant={variant}
+          icon={icon}
+          className={className}
+          onClick={onClick}
+          type={type}
+          disabled={disabled}
+        >
+          {children}
+        </ButtonComponent>
+      </Link>
+    ) : tooltip ? (
+      <Tooltip message={tooltip ?? ""}>
+        <ButtonComponent
+          variant={variant}
+          icon={icon}
+          className={className}
+          onClick={onClick}
+          type={type}
+          disabled={disabled}
+        >
+          {children}
+        </ButtonComponent>
+      </Tooltip>
+    ) : (
+      <ButtonComponent />
+    );
+  }
+);
+
+interface ButtonComponentProps {
+  children?: React.ReactNode;
+  onClick?: () => void;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link";
+  type?: "button" | "submit" | "reset";
+  disabled?: boolean;
+  className?: string;
+  icon?: keyof typeof iconMap;
+  iconComponent?: React.ComponentType<{ className?: string }>;
+}
+
+const ButtonComponent = ({
   children,
   onClick,
   variant,
   type,
   disabled,
-  href,
   className,
-  pending,
-}: ButtonProps) {
-  return pending ? (
-    <LoadingButton
-      variant={variant}
-      className={cn("font-bold text-sm border ", className)}
-    >
-      {children}
-    </LoadingButton>
-  ) : href && !pending ? (
-    <ShadButton
-      asChild
-      className={cn("font-bold text-sm border ", className)}
-      variant={variant}
-    >
-      <Link href={href}>{children}</Link>
-    </ShadButton>
-  ) : (
+  icon,
+  iconComponent,
+}: ButtonComponentProps) => {
+  const Icon = iconComponent || (icon ? iconMap[icon] : null);
+
+  return (
     <ShadButton
       variant={variant}
       onClick={onClick}
       type={type}
       disabled={disabled}
-      className={cn("font-bold text-sm border ", className)}
+      className={cn(
+        "group group-hover:text-primary  group-hover:cursor-pointer font-bold text-sm border flex gap-2",
+        className,
+        !children &&
+          "bg-transparent border-none group-hover:bg-transparent hover:bg-transparent "
+      )}
     >
       {children}
+      {Icon && (
+        <Icon
+          className={cn(
+            "text-gray-100 transform transition-ease-in-out duration-300 ease-in-out w-5 h-5",
+            className,
+            !children &&
+              "group-hover:text-primary text-gray-500 group-hover:cursor-pointer hover:text-primary hover:cursor-pointer group-hover:scale-105 transform ease-in-out duration-300 w-7 h-7"
+          )}
+        />
+      )}
     </ShadButton>
   );
-}
+};
