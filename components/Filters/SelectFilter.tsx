29acd5aee2a116aa41aa6701d6d691652150e218
Vigee <contact@vigee.fr>
2024-04-18 20:30:39 +0200

    fix drawermobile for scroll select

--- components/Filters/SelectFilter.tsx
+++ components/Filters/SelectFilter.tsx
@@ -1,11 +1,10 @@
 "use client";
-import React from "react";
+import React, { useCallback } from "react";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Select } from "../../components/Select/Select";
 
 interface Props {
   statusName: string;
-  defaultYear?: string; // Ajout d'une prop pour l'année par défaut
   placeholder?: string;
   status: { label: string; value: string }[];
 }
@@ -13,24 +12,35 @@ interface Props {
 export const SelectFilter = ({ statusName, status, placeholder }: Props) => {
   const { replace } = useRouter();
   const pathname = usePathname();
-  const [selectedStatus, setSelectedStatus] = React.useState<string>("");
   const searchParams = useSearchParams();
-  const handleSelectStatus = (status: string) => {
-    const params = new URLSearchParams(searchParams);
-    params.set(statusName, status);
-    params.set("page", "1");
-    replace(`${pathname}?${params.toString()}`);
-  };
+
+  const handleSearch = useCallback(
+    (status: string | undefined) => {
+      const params = new URLSearchParams(searchParams.toString());
+      params.set("page", "1");
+
+      if (
+        status === undefined ||
+        status === searchParams.get(statusName) ||
+        status === ""
+      ) {
+        params.delete(statusName);
+      } else {
+        params.set(statusName, status);
+      }
+
+      replace(`${pathname}?${params.toString()}`);
+    },
+    [searchParams, statusName]
+  );
 
   return (
-      <Select
-          className="w-full md:w-auto"
-          onChange={(selectedValue) => {
-            setSelectedStatus(selectedValue ?? "");
-            handleSelectStatus(selectedValue ?? "");
-          }}
-          placeholder={placeholder}
-          options={status}
-      />
+    <Select
+      className="w-full md:w-auto"
+      onChange={handleSearch}
+      defaultValue={searchParams.get(statusName) ?? ""}
+      placeholder={placeholder}
+      options={status}
+    />
   );
 };
