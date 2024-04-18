866136ed13b2fb08dbb1dacf0aa252d9432d7625
Vigee <contact@vigee.fr>
2024-04-18 07:31:58 +0200

    deleted old buttons

--- components/Buttons/Button.tsx
+++ components/Buttons/Button.tsx
@@ -74,26 +74,24 @@ export const Button = React.memo<ButtonProps>(
           onClick={onClick}
           type={type}
           disabled={disabled}
+          tooltip={tooltip}
         >
           {children}
         </ButtonComponent>
       </Link>
-    ) : tooltip ? (
-      <Tooltip message={tooltip ?? ""}>
-        <ButtonComponent
-          variant={variant}
-          icon={icon}
-          iconComponent={iconComponent}
-          className={className}
-          onClick={onClick}
-          type={type}
-          disabled={disabled}
-        >
-          {children}
-        </ButtonComponent>
-      </Tooltip>
     ) : (
-      <ButtonComponent />
+      <ButtonComponent
+        variant={variant}
+        icon={icon}
+        iconComponent={iconComponent}
+        className={className}
+        onClick={onClick}
+        type={type}
+        disabled={disabled}
+        tooltip={tooltip}
+      >
+        {children}
+      </ButtonComponent>
     );
   }
 );
@@ -113,6 +111,7 @@ interface ButtonComponentProps {
   className?: string;
   icon?: keyof typeof iconMap;
   iconComponent?: React.ComponentType<{ className?: string }>;
+  tooltip?: string;
 }
 
 const ButtonComponent = ({
@@ -124,10 +123,38 @@ const ButtonComponent = ({
   className,
   icon,
   iconComponent,
+  tooltip,
 }: ButtonComponentProps) => {
   const Icon = iconComponent || (icon ? iconMap[icon] : null);
 
-  return (
+  return tooltip ? (
+    <Tooltip message={tooltip ?? ""}>
+      <ShadButton
+        variant={variant}
+        onClick={onClick}
+        type={type}
+        disabled={disabled}
+        className={cn(
+          "group group-hover:text-primary  group-hover:cursor-pointer font-bold text-sm border flex gap-2",
+          className,
+          !children &&
+            "bg-transparent border-none group-hover:bg-transparent hover:bg-transparent "
+        )}
+      >
+        {children}
+        {Icon && (
+          <Icon
+            className={cn(
+              "text-gray-100 transform transition-ease-in-out duration-300 ease-in-out w-5 h-5",
+              className,
+              !children &&
+                "group-hover:text-primary text-gray-400 group-hover:cursor-pointer hover:text-primary hover:cursor-pointer group-hover:scale-105 transform transition-ease-in-out duration-300 w-7 h-7"
+            )}
+          />
+        )}
+      </ShadButton>
+    </Tooltip>
+  ) : (
     <ShadButton
       variant={variant}
       onClick={onClick}
