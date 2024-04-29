8bf8f5172449701aac3f2ca877afe1bef5867676
Vigee <contact@vigee.fr>
2024-04-29 10:05:11 +0200

    sheet and button

--- components/Buttons/Button.tsx
+++ components/Buttons/Button.tsx
@@ -152,21 +152,22 @@ const ButtonComponent = ({
         )}
       >
         {children}
-        {iconComponent
-          ? iconComponent
-          : Icon && (
-              <Icon
-                className={cn(
-                  "text-gray-100 transform transition-ease-in-out duration-300 ease-in-out w-5 h-5",
-                  className,
-                  !children &&
-                    "group-hover:text-primary text-gray-400 group-hover:cursor-pointer hover:text-primary hover:cursor-pointer group-hover:scale-105 transform transition-ease-in-out duration-300 w-7 h-7",
-                  big && "w-10 h-10",
-                  variant === "outline" && "text-gray-900",
-                  variant === "secondary" && "text-secondary-foreground"
-                )}
-              />
+
+        {iconComponent}
+
+        {Icon && (
+          <Icon
+            className={cn(
+              "text-gray-100 transform transition-ease-in-out duration-300 ease-in-out w-5 h-5",
+              className,
+              !children &&
+                "group-hover:text-primary text-gray-400 group-hover:cursor-pointer hover:text-primary hover:cursor-pointer group-hover:scale-105 transform transition-ease-in-out duration-300 w-7 h-7",
+              big && "w-10 h-10",
+              variant === "outline" && "text-gray-900",
+              variant === "secondary" && "text-secondary-foreground"
             )}
+          />
+        )}
       </ShadButton>
     </Tooltip>
   ) : (
@@ -183,20 +184,21 @@ const ButtonComponent = ({
       )}
     >
       {children}
-      {iconComponent
-        ? iconComponent
-        : Icon && (
-            <Icon
-              className={cn(
-                "text-gray-100 transform transition-ease-in-out duration-300 ease-in-out w-5 h-5",
-                !children &&
-                  "group-hover:text-primary text-gray-400 group-hover:cursor-pointer hover:text-primary hover:cursor-pointer group-hover:scale-105 transform transition-ease-in-out duration-300 w-7 h-7",
-                big && "w-10 h-10",
-                variant === "outline" && "text-gray-900",
-                variant === "secondary" && "text-secondary-foreground"
-              )}
-            />
+      {iconComponent}
+
+      {Icon && (
+        <Icon
+          className={cn(
+            "text-gray-100 transform transition-ease-in-out duration-300 ease-in-out w-5 h-5",
+            className,
+            !children &&
+              "group-hover:text-primary text-gray-400 group-hover:cursor-pointer hover:text-primary hover:cursor-pointer group-hover:scale-105 transform transition-ease-in-out duration-300 w-7 h-7",
+            big && "w-10 h-10",
+            variant === "outline" && "text-gray-900",
+            variant === "secondary" && "text-secondary-foreground"
           )}
+        />
+      )}
     </ShadButton>
   );
 };
