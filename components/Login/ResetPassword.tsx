7c739c38187f5cf839e553b61e3c0e217ecce1a2
Julien <35800390+JulienEmrot@users.noreply.github.com>
2024-05-02 09:28:00 +0200

    reset password

--- components/Login/ResetPassword.tsx
+++ components/Login/ResetPassword.tsx
@@ -1,37 +1,41 @@
+import React from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { useResetPassword } from "../../hooks/password/usePassword";
+import { useRouter } from "next/navigation";
+import Image, { StaticImageData } from "next/image";
+import {TypographyH1} from "../Typography/Typography";
+import {Form} from "../ui/form";
+import Input from "../Forms/Input";
+import {Button} from "../Buttons/Button";
 
 type ResetPasswordProps = {
   token: string;
+  logo?: StaticImageData;
 };
 
-export default function ResetPassword({ token }: ResetPasswordProps) {
+export default function ResetPassword({ token, logo }: ResetPasswordProps) {
+  const router = useRouter();
   type FormValues = {
     password: string;
     password_confirmation: string;
   };
 
   const schema = z
-    .object({
-      password: z
-        .string()
-        .min(6, "Le mot de passe doit comporter au moins 6 caractères."),
-      password_confirmation: z.string(),
-    })
-    .refine(data => data.password === data.password_confirmation, {
-      message: "Les mots de passe doivent correspondre.",
-      path: ["password_confirmation"], // précisez le chemin de l'erreur
-    });
+      .object({
+        password: z
+            .string()
+            .min(6, "Le mot de passe doit comporter au moins 6 caractères."),
+        password_confirmation: z.string(),
+      })
+      .refine((data) => data.password === data.password_confirmation, {
+        message: "Les mots de passe doivent correspondre.",
+        path: ["password_confirmation"], // précisez le chemin de l'erreur
+      });
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm<FormValues>({
+  const form = useForm<FormValues>({
     resolver: zodResolver(schema),
   });
 
@@ -41,63 +45,62 @@ export default function ResetPassword({ token }: ResetPasswordProps) {
     try {
       await mutation.mutateAsync({ ...data, token });
       toast.success("Mot de passe réinitialisé avec succès !");
-      reset();
+      router.push("/login");
     } catch (error) {
       toast.error(
-        "Une erreur est survenue lors de la réinitialisation du mot de passe."
+          "Une erreur est survenue lors de la réinitialisation du mot de passe.",
       );
     }
   };
 
   return (
-    <div className="w-full max-w-sm space-y-6 align-center my-auto justify-center mx-auto">
-      <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label htmlFor="password" className="sr-only">
-            Nouveau mot de passe
-          </label>
-          <input
-            {...register("password")}
-            id="password"
-            type="password"
-            required
-            className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
-            placeholder="Nouveau mot de passe"
-          />
-          {errors.password && (
-            <span className="text-red-500">{errors.password.message}</span>
+      <>
+        <div className="w-full max-w-xl space-y-6 align-center my-auto justify-start mx-auto py-40 md:px-8 md:p-24 md:py-4 min-h-screen md:min-h-fit md:max-w-[480px]">
+          {logo && (
+              <Image
+                  width={100}
+                  height={100}
+                  className=" ml-4 md:ml-4 absolute md:relative top-12 md:top-0"
+                  src={logo}
+                  alt="Vigee - Make IT Simple"
+              />
           )}
-        </div>
+          <div className="flex flex-col justify-start mx-auto md:shadow-sm  md:border border-gray-100 rounded-xl px-4 md:p-8  md:bg-white">
+            <div>
+              <TypographyH1 className="text-primary pb-4">
+                Réinitialiser mon mot de passe
+              </TypographyH1>
+            </div>
 
-        <div>
-          <label htmlFor="password_confirmation" className="sr-only">
-            Confirmez le mot de passe
-          </label>
-          <input
-            {...register("password_confirmation")}
-            id="password_confirmation"
-            type="password"
-            required
-            className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
-            placeholder="Confirmez le mot de passe"
-          />
-          {errors.password_confirmation && (
-            <span className="text-red-500">
-              {errors.password_confirmation.message}
-            </span>
-          )}
-        </div>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <Input
+                    form={form}
+                    name="password"
+                    type="password"
+                    label="Nouveau mot de passe"
+                />
 
-        <div>
-          <button
-            type="submit"
-            disabled={mutation.isPending}
-            className="flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
-          >
-            Réinitialiser mon mot de passe
-          </button>
+                <Input
+                    form={form}
+                    name="password_confirmation"
+                    type="password"
+                    label="Confirmez le mot de passe"
+                />
+
+                <div>
+                  <Button
+                      type="submit"
+                      pending={mutation.isPending}
+                      className="flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-secondary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
+                  >
+                    Réinitialiser mon mot de passe
+                  </Button>
+                </div>
+              </form>
+            </Form>
+          </div>
         </div>
-      </form>
-    </div>
+      </>
   );
 }
