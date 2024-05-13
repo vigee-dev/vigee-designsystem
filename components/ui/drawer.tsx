96a9a9363c5bcd6a8d6800cb619ec5a05887d43f
Vigee <contact@vigee.fr>
2024-05-13 16:50:38 +0200

    drawer fix

--- components/ui/drawer.tsx
+++ components/ui/drawer.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import { Drawer as DrawerPrimitive } from "vaul";
 
-import { cn } from "../lib/utils";
+import { cn } from "../../components/lib/utils";
 
 const Drawer = ({
   shouldScaleBackground = true,
@@ -43,7 +43,7 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-x-0 bottom-0 z-50 mt-24 flex max-h-[90vh] flex-col rounded-t-[10px] border bg-background",
+        "fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[10px] border bg-background",
         className
       )}
       {...props}
