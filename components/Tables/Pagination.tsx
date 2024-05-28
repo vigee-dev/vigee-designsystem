55f41f417411a1a1c0ac04fd8defd7c35a5b7b09
Vigee <contact@vigee.fr>
2024-05-28 16:54:46 +0200

    Update Pagination.tsx

--- components/Tables/Pagination.tsx
+++ components/Tables/Pagination.tsx
@@ -77,7 +77,7 @@ function PaginationNumber({
     {
       "rounded-l-md": position === "first" || position === "single",
       "rounded-r-md": position === "last" || position === "single",
-      "z-10 bg-primary border-primary text-white": isActive,
+      " bg-primary border-primary text-white": isActive,
       "hover:bg-gray-100": !isActive && position !== "middle",
       "text-gray-300": position === "middle",
     }
