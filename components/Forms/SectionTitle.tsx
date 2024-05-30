6b62aad49b0c325c504ba2b6b1b001fb9fbdd605
Vigee <contact@vigee.fr>
2024-05-30 14:51:12 +0200

    Added component for form section titles

---
+++ components/Forms/SectionTitle.tsx
@@ -0,0 +1,15 @@
+import { TypographyH2 } from "../Typography/Typography";
+
+interface SectionTitleProps {
+  title: string;
+  subtitle: string;
+}
+
+export const SectionTitle = ({ title, subtitle }: SectionTitleProps) => {
+  return (
+    <div className="flex flex-col">
+      <TypographyH2 className="text-primary font-medium">{title}</TypographyH2>
+      <p className="text-sm text-gray-400">{subtitle}</p>
+    </div>
+  );
+};
