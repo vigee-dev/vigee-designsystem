07ca397799648b49c8aa4ff663270cece513ec32
Vigee <contact@vigee.fr>
2024-05-30 14:53:28 +0200

    Update SectionTitle.tsx

--- components/Forms/SectionTitle.tsx
+++ components/Forms/SectionTitle.tsx
@@ -2,14 +2,16 @@ import { TypographyH2 } from "../Typography/Typography";
 
 interface SectionTitleProps {
   title: string;
-  subtitle: string;
+  subtitle?: string;
 }
 
 export const SectionTitle = ({ title, subtitle }: SectionTitleProps) => {
   return (
     <div className="flex flex-col">
       <TypographyH2 className="text-primary font-medium">{title}</TypographyH2>
-      <p className="text-sm text-gray-400">{subtitle}</p>
+      <p className="text-sm text-gray-400 font-disaply font-variations">
+        {subtitle}
+      </p>
     </div>
   );
 };
