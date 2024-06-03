8e871b61426a34d85ad0292d153a03d39f3a58b3
Vigee <contact@vigee.fr>
2024-06-03 22:37:58 +0200

    Update Avatar.tsx

--- components/Avatar/Avatar.tsx
+++ components/Avatar/Avatar.tsx
@@ -1,30 +1,43 @@
-import {Avatar as ShadcnAvatar, AvatarFallback, AvatarImage} from "../ui/avatar";
-import {PiUserCircleDuoSolid} from "../../icons/PikaIcons";
+import {
+  Avatar as ShadcnAvatar,
+  AvatarFallback,
+  AvatarImage,
+} from "../ui/avatar";
+import { PiUserCircleDuoSolid } from "../../icons/PikaIcons";
 import React from "react";
-import {cn} from "../lib/utils";
+import { cn } from "../lib/utils";
 
 interface Props {
-  src?: string | null,
-  alt?: string
-  onClick?: () => void
-  className?: string
+  src?: string | null;
+  alt?: string;
+  onClick?: () => void;
+  className?: string;
 }
 
 const Avatar = ({ src, alt, onClick, className }: Props) => {
   return (
     <ShadcnAvatar
-      className={cn("border shadow w-16 h-16 flex justify-center items-center", className)}
+      className={cn(
+        "border shadow w-16 h-16 flex justify-center items-center",
+        className
+      )}
       onClick={onClick}
     >
       {src != null ? (
-        <AvatarImage src={src} alt={alt || 'Photo de profil'} />
+        <AvatarImage
+          src={src}
+          alt={alt || "Photo de profil"}
+          className="object-cover"
+        />
       ) : (
         <AvatarFallback>
-          <PiUserCircleDuoSolid className={cn("text-gray-400 w-full h-full", className)} />
+          <PiUserCircleDuoSolid
+            className={cn("text-gray-400 w-full h-full", className)}
+          />
         </AvatarFallback>
       )}
     </ShadcnAvatar>
-  )
-}
+  );
+};
 
-export default Avatar
+export default Avatar;
