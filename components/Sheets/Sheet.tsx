7b2ae45e508758b9511832daf078bcfe3134d1c5
Vigee <contact@vigee.fr>
2024-06-05 10:18:11 +0200

    Update Sheet.tsx

--- components/Sheets/Sheet.tsx
+++ components/Sheets/Sheet.tsx
@@ -29,13 +29,17 @@ export default function SheetTriggered({
 }: Props) {
   return (
     <Sheet>
-      <SheetTrigger>{trigger}</SheetTrigger>
+      <SheetTrigger className="w-full">{trigger}</SheetTrigger>
       <SheetContent side={side}>
         <SheetHeader className="text-left flex pb-4">
           <SheetTitle className="text-primary">{title}</SheetTitle>
           <SheetDescription>{description}</SheetDescription>
         </SheetHeader>
         {children}
+
+        <SheetClose className="w-full flex justify-center items-center text-center pt-10">
+          <Button icon="chevronDown" />
+        </SheetClose>
       </SheetContent>
     </Sheet>
   );
