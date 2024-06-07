4e18febfe5f61cbbf3eefecd5355ad0345d294fc
Vigee <contact@vigee.fr>
2024-06-07 11:05:32 +0200

    Update Sheet.tsx

--- components/Sheets/Sheet.tsx
+++ components/Sheets/Sheet.tsx
@@ -32,6 +32,7 @@ interface Props {
   side?: "left" | "right" | "top" | "bottom";
   size?: "sm" | "md" | "lg";
   scroll?: boolean;
+  sheet?: boolean;
 }
 
 type SheetContextType = {
@@ -62,12 +63,13 @@ export default function SheetTriggered({
   icon,
   size = "md",
   scroll = true,
+  sheet = false,
 }: Props) {
   const [open, setOpen] = React.useState(false);
   const contextValue: SheetContextType = { open, setOpen };
   const isDesktop = useMediaQuery("(min-width: 768px)");
 
-  if (isDesktop) {
+  if (isDesktop || !sheet) {
     return (
       <SheetContext.Provider value={contextValue}>
         <div>
@@ -96,7 +98,14 @@ export default function SheetTriggered({
                 </div>
               </DialogHeader>
 
-              <div className={cn("p-1 max-h-[80vh]", scroll && 'overflow-y-scroll')}> {children}</div>
+              <div
+                className={cn(
+                  "p-1 max-h-[80vh]",
+                  scroll && "overflow-y-scroll"
+                )}
+              >
+                {children}
+              </div>
             </DialogContent>
           </Dialog>
         </div>
@@ -113,9 +122,7 @@ export default function SheetTriggered({
 
         <SheetContent
           side={side}
-          className={
-            `rounded-t-2xl lg:max-w-screen-lg overflow-y-scroll max-h-[90vh]`
-          }
+          className={`rounded-t-2xl lg:max-w-screen-lg overflow-y-scroll max-h-[90vh]`}
           onClick={e => e.stopPropagation()}
         >
           <SheetHeader className="text-left flex pb-4">
@@ -123,7 +130,12 @@ export default function SheetTriggered({
             <SheetDescription>{description}</SheetDescription>
           </SheetHeader>
 
-          <div className={cn("p-1 max-h-[80vh] ", scroll && 'overflow-y-scroll')}> {children}</div>
+          <div
+            className={cn("p-1 max-h-[80vh] ", scroll && "overflow-y-scroll")}
+          >
+            {" "}
+            {children}
+          </div>
 
           <SheetClose className="w-full flex justify-center items-center text-center pt-2">
             <Button icon="chevronDown" />
