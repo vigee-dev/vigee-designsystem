ba193d8fea8473503d568951723940f0fa034930
Vigee <contact@vigee.fr>
2024-06-08 15:42:11 +0200

    Update TimePicker.tsx

--- components/Forms/TimePicker.tsx
+++ components/Forms/TimePicker.tsx
@@ -59,8 +59,8 @@ const generateTimeOptions = (
   for (let i = 0; i < 24; i++) {
     for (let j = 0; j < 60; j += step) {
       if (
-        (i > minHour || (i === minHour && j >= minMinute)) &&
-        (i < maxHour || (i === maxHour && j <= maxMinute))
+        (i >= minHour || (i === minHour && j >= minMinute)) &&
+        (i <= maxHour || (i === maxHour && j <= maxMinute))
       ) {
         const hour = i.toString().padStart(2, "0");
         const minute = j.toString().padStart(2, "0");
