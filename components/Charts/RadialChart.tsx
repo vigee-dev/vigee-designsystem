6bfa391f6a8217444689923a405a1a5fb8cd3736
Vigee <contact@vigee.fr>
2024-07-08 17:29:44 +0200

    Update RadialChart.tsx

--- components/Charts/RadialChart.tsx
+++ components/Charts/RadialChart.tsx
@@ -32,7 +32,7 @@ const chartConfig = {
   },
 } satisfies ChartConfig;
 
-export function Component() {
+export function RadialChart() {
   return (
     <Card className="flex flex-col">
       <CardHeader className="items-center pb-0">
