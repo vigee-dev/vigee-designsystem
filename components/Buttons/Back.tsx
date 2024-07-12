5b31ceeabc1d584246f417dc99a243994202faa4
Julien <35800390+JulienEmrot@users.noreply.github.com>
2024-07-12 18:04:36 +0200

    change Back.tsx

--- components/Buttons/Back.tsx
+++ components/Buttons/Back.tsx
@@ -6,30 +6,45 @@ interface BackProps {
   href?: string;
   where?: string;
   onClick?: () => void;
+  linkBtn?: boolean;
 }
 
-const Back: React.FC<BackProps> = ({ href, where, onClick }) => {
+const Back: React.FC<BackProps> = ({
+                                     href,
+                                     where,
+                                     onClick,
+                                     linkBtn = false,
+                                   }) => {
   return (
-    <div className="flex flex-grid text-gray-400 ">
-      {href ? (
-        <Link
-          href={href}
-          className="flex flex-grid text-gray-400 pb-1 hover:text-primary transform ease-in-out duration-200 hover:font-bold"
-        >
-          <ArrowLeftIcon width={15} />
-          <p className="mx-1 text-sm "> {where}</p>
-        </Link>
-      ) : (
-        <Button
-          variant="ghost"
-          onClick={onClick}
-          className="flex flex-grid text-gray-400 pb-1 hover:text-primary transform ease-in-out duration-200 hover:font-bold border-0"
-        >
-          <ArrowLeftIcon width={15} />
-          <p className="mx-1 text-sm "> {where}</p>
-        </Button>
-      )}
-    </div>
+      <div className="flex flex-grid text-gray-400 ">
+        {href ? (
+            <Link
+                href={href}
+                className="flex flex-grid text-gray-400 pb-1 hover:text-primary transform ease-in-out duration-200 hover:font-bold"
+            >
+              <ArrowLeftIcon width={15} />
+              <p className="mx-1 text-sm "> {where}</p>
+            </Link>
+        ) : !linkBtn ? (
+            <Button
+                variant="ghost"
+                onClick={onClick}
+                className="flex flex-grid text-gray-400 pb-1 hover:text-primary transform ease-in-out duration-200 hover:font-bold border-0"
+            >
+              <ArrowLeftIcon width={15} />
+              <p className="mx-1 text-sm "> {where}</p>
+            </Button>
+        ) : (
+            <Button
+                variant="ghost"
+                onClick={onClick}
+                className="flex flex-grid text-gray-400 pb-1 hover:text-primary transform ease-in-out duration-200 hover:font-bold font-normal gap-0 hover:bg-transparent"
+            >
+              <ArrowLeftIcon width={15} />
+              <p className="mx-1 text-sm "> {where}</p>
+            </Button>
+        )}
+      </div>
   );
 };
 
