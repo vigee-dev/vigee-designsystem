cfe7ff50aee6f8511100c71d8e32016173d0e178
Julien <35800390+JulienEmrot@users.noreply.github.com>
2024-07-16 16:51:41 +0200

    update switch to add children

--- components/Forms/Switch.tsx
+++ components/Forms/Switch.tsx
@@ -10,10 +10,11 @@ import { UseFormReturn, FieldValues, Path } from "react-hook-form";
 
 import { Switch as SwitchShadcn } from "../ui/switch";
 import { cn } from "../lib/utils";
+import Link from "next/link";
 
 type Props<T extends FieldValues> = {
   form?: UseFormReturn<T>;
-  label: string;
+  label?: string;
   placeholder?: string;
   required?: boolean;
   name?: Path<T>;
@@ -21,60 +22,62 @@ type Props<T extends FieldValues> = {
   onChange?: (value: boolean) => void;
   value?: boolean;
   className?: string;
+  children?: React.ReactNode;
 };
 
 export default function Switch<T extends FieldValues>({
-  form,
-  label,
-  name,
-  descr,
-  className,
-  onChange,
-  value,
-}: Props<T>) {
+                                                        form,
+                                                        label,
+                                                        name,
+                                                        descr,
+                                                        className,
+                                                        onChange,
+                                                        value,
+                                                        children,
+                                                      }: Props<T>) {
   return form?.control && name ? (
-    <FormField
-      control={form.control}
-      name={name}
-      render={({ field }) => (
-        <FormItem
-          className={cn(
-            "flex flex-row items-center justify-between py-4 border rounded-xl p-4  text-primary bg-white",
-            className
+      <FormField
+          control={form.control}
+          name={name}
+          render={({ field }) => (
+              <FormItem
+                  className={cn(
+                      "flex flex-row items-center justify-between py-4 border rounded-xl p-4  text-primary bg-white",
+                      className,
+                  )}
+              >
+                <div className="space-y-0.5">
+                  <FormLabel className="text-base ">{children ? children : label}</FormLabel>
+                  <FormDescription>{descr}</FormDescription>
+                </div>
+                <FormControl>
+                  <SwitchShadcn
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                      className="data-[state=unchecked]:bg-slate-200"
+                  />
+                </FormControl>
+              </FormItem>
           )}
-        >
-          <div className="space-y-0.5">
-            <FormLabel className="text-base ">{label}</FormLabel>
-            <FormDescription>{descr}</FormDescription>
-          </div>
-          <FormControl>
-            <SwitchShadcn
-              checked={field.value}
-              onCheckedChange={field.onChange}
-              className="data-[state=unchecked]:bg-slate-200"
-            />
-          </FormControl>
-        </FormItem>
-      )}
-    />
+      />
   ) : (
-    <FormItem
-      className={cn(
-        "flex flex-row items-center justify-between py-4 border rounded-xl p-4  text-primary bg-white",
-        className
-      )}
-    >
-      <div className="space-y-0.5">
-        <FormLabel className="text-base ">{label}</FormLabel>
-        <FormDescription>{descr}</FormDescription>
-      </div>
-      <FormControl>
-        <SwitchShadcn
-          checked={value}
-          onCheckedChange={onChange}
-          className="data-[state=unchecked]:bg-slate-200"
-        />
-      </FormControl>
-    </FormItem>
+      <FormItem
+          className={cn(
+              "flex flex-row items-center justify-between py-4 border rounded-xl p-4  text-primary bg-white",
+              className,
+          )}
+      >
+        <div className="space-y-0.5">
+          <FormLabel className="text-base ">{label}</FormLabel>
+          <FormDescription>{descr}</FormDescription>
+        </div>
+        <FormControl>
+          <SwitchShadcn
+              checked={value}
+              onCheckedChange={onChange}
+              className="data-[state=unchecked]:bg-slate-200"
+          />
+        </FormControl>
+      </FormItem>
   );
 }
