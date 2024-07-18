8e555822b849ca19adac5ba768d26b81919192b8
Vigee <contact@vigee.fr>
2024-07-18 16:00:22 +0200

    Update Error.tsx

--- components/Errors/Error.tsx
+++ components/Errors/Error.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
 import error from "../../img/error/error.svg";
 import Link from "next/link";
@@ -10,41 +10,19 @@ interface IllustrationProps {
   subtitle?: string;
   children?: React.ReactNode;
   supportEmail?: string;
+  img?: StaticImageData;
   onClick?: () => void;
 }
-export function Error({
-  title,
-  subtitle,
-  children,
-  supportEmail,
-  onClick,
-}: IllustrationProps) {
+export function Error({ title, subtitle, children, supportEmail, onClick, img = error }: IllustrationProps) {
   return (
     <div className="flex flex-col items-center justify-center  w-full px-12 ">
-      {error && (
-        <Image
-          width={400}
-          height={400}
-          className="mx-auto w-44 h-auto"
-          src={error}
-          alt="Erreur"
-        />
-      )}
-      <h1
-        className={
-          "text-xl text-primary font-bold text-center pt-6 font-display"
-        }
-      >
-        {title ? title : "Une erreur s'est produite."}
-      </h1>
+      {img && <Image width={400} height={400} className="mx-auto w-44 h-auto" src={img} alt="Erreur" />}
+      <h1 className={"text-xl text-primary font-bold text-center pt-6 font-display"}>{title ? title : "Une erreur s'est produite."}</h1>
 
       {subtitle && <p className="text-gray-400">{subtitle}</p>}
 
       {supportEmail && (
-        <Link
-          className="text-sm text-gray-500  items-center text-center font-display"
-          href={supportEmail ? `mailto:${supportEmail}` : "/"}
-        >
+        <Link className="text-sm text-gray-500  items-center text-center font-display" href={supportEmail ? `mailto:${supportEmail}` : "/"}>
           <Button variant="outline" className=" flex gap-x-2">
             Contacter le support <PiEnvelopeArrowRightDuoSolid />
           </Button>
