b64cf4f275d3d0e282bba6dfc5e248c35cdc4fb4
Vigee <contact@vigee.fr>
2024-07-18 16:08:38 +0200

    unauthorized

---
+++ components/Errors/Unauthorized.tsx
@@ -0,0 +1,41 @@
+import Image, { StaticImageData } from "next/image";
+
+import unauthorized from "../../img/error/unauthorized.svg";
+import Link from "next/link";
+import { PiEnvelopeArrowRightDuoSolid } from "../../icons/PikaIcons";
+import { Button } from "../Buttons/Button";
+
+interface IllustrationProps {
+  title?: string;
+  subtitle?: string;
+  children?: React.ReactNode;
+  supportEmail?: string;
+  img?: StaticImageData;
+  onClick?: () => void;
+}
+export function Unauthorized({ title = "Accès refusé", subtitle = "Vous n'avez pas les droits pour accéder à cette page.", children, supportEmail, onClick, img = unauthorized }: IllustrationProps) {
+  return (
+    <div className="flex flex-col items-center justify-center  w-full px-12 ">
+      {img && <Image width={400} height={400} className="mx-auto w-44 h-auto" src={img} alt="Erreur" />}
+      <h1 className={"text-xl text-primary font-bold text-center pt-6 font-display"}>{title ? title : "Une erreur s'est produite."}</h1>
+
+      {subtitle && <p className="text-gray-400">{subtitle}</p>}
+
+      {supportEmail && (
+        <Link className="text-sm text-gray-500  items-center text-center font-display" href={supportEmail ? `mailto:${supportEmail}` : "/"}>
+          <Button variant="outline" className=" flex gap-x-2">
+            Contacter le support <PiEnvelopeArrowRightDuoSolid />
+          </Button>
+        </Link>
+      )}
+
+      {children}
+
+      {onClick && (
+        <Button onClick={onClick} className="py-2">
+          Rééssayer
+        </Button>
+      )}
+    </div>
+  );
+}
