2397295b78718a8591d891ec299f40291c1ade7e
Vigee <contact@vigee.fr>
2024-07-22 19:10:38 +0200

    fix dep

--- components/Illustration/EmptyIllustration.tsx
+++ components/Illustration/EmptyIllustration.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-
 import Link from "next/link";
 import { Button } from "../Buttons/Button";
 import { Container } from "../Container/Container";
--- components/Illustration/Illustration.tsx
+++ components/Illustration/Illustration.tsx
@@ -9,39 +9,14 @@ interface IllustrationProps {
   width?: number;
   height?: number;
 }
-export default function Illustration({
-  title,
-  title2,
-  subtitle,
-  img,
-  children,
-  width = 500,
-  height = 500,
-}: IllustrationProps) {
+export default function Illustration({ title, title2, subtitle, img, children, width = 500, height = 500 }: IllustrationProps) {
   return (
     <div className="flex flex-col items-center justify-center bg-primary h-screen w-full px-12 ">
-      {img && (
-        <Image
-          width={width}
-          height={height}
-          className={`mx-auto`}
-          src={img}
-          alt="LoginForm image"
-        />
-      )}
-      <h1
-        className={
-          "text-5xl text-gray-300 font-black text-center pt-6 font-display"
-        }
-      >
+      {img && <Image width={width} height={height} className={`mx-auto`} src={img} alt="LoginForm image" />}
+      <h1 className={"text-5xl text-gray-300 font-black text-center pt-6 font-display"}>
         {title} <span className="text-white">{title2}</span>
       </h1>
-      {subtitle && (
-        <p className="text-xl text-slate-500   text-center font-display">
-          {" "}
-          {subtitle}{" "}
-        </p>
-      )}
+      {subtitle && <p className="text-xl text-slate-500   text-center font-display"> {subtitle} </p>}
 
       {children}
     </div>
