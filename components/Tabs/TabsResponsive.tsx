fe17075d2238901d84baa3da4afe34ca46ce0813
Vigee <contact@vigee.fr>
2024-07-23 17:33:23 +0200

    Update TabsResponsive.tsx

--- components/Tabs/TabsResponsive.tsx
+++ components/Tabs/TabsResponsive.tsx
@@ -1,16 +1,10 @@
 "use client";
 import { ReactNode } from "react";
 import { useQueryState } from "nuqs";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "../../components/ui/select";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../../components/ui/select";
 import { Tabs, TabsTrigger, TabsList } from "../../components/ui/tabs";
 import { cn } from "../lib/utils";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Badge } from "../ui/badge";
 
 interface TabsResponsiveProps {
@@ -30,15 +24,7 @@ interface TabsResponsiveProps {
   selectLimit?: number;
 }
 
-export const TabsResponsive = ({
-  options,
-  defaultValue,
-  query,
-  children,
-  fullWidth,
-  className,
-  selectLimit = 4,
-}: TabsResponsiveProps) => {
+export const TabsResponsive = ({ options, defaultValue, query, children, fullWidth, className, selectLimit = 4 }: TabsResponsiveProps) => {
   const router = useRouter();
 
   const [filter, setFilter] = useQueryState(query ?? "", {
@@ -47,14 +33,18 @@ export const TabsResponsive = ({
     shallow: false,
   });
 
-  const handleValueChange = (
-    value: string,
-    option: { href?: string; value?: string }
-  ) => {
+  const [page, setPage] = useQueryState("page", {
+    defaultValue: "1",
+    clearOnDefault: false,
+    shallow: false,
+  });
+
+  const handleValueChange = (value: string, option: { href?: string; value?: string }) => {
     if (option.href) {
       router.push(option.href);
     } else if (option.value && query) {
       setFilter(value);
+      setPage("1");
     }
   };
 
@@ -62,41 +52,20 @@ export const TabsResponsive = ({
     <div>
       <div className="hidden md:flex w-full ">
         {options.length < 7 ? (
-          <TabComponent
-            options={options}
-            defaultValue={defaultValue}
-            handleValueChange={handleValueChange}
-            fullWidth={fullWidth}
-            className={className}
-          >
+          <TabComponent options={options} defaultValue={defaultValue} handleValueChange={handleValueChange} fullWidth={fullWidth} className={className}>
             {children}
           </TabComponent>
         ) : (
-          <SelectComponent
-            options={options}
-            defaultValue={defaultValue}
-            handleValueChange={handleValueChange}
-            className={className}
-          />
+          <SelectComponent options={options} defaultValue={defaultValue} handleValueChange={handleValueChange} className={className} />
         )}
       </div>
       <div className="flex md:hidden">
         {options.length < selectLimit ? (
-          <TabComponent
-            options={options}
-            defaultValue={defaultValue}
-            handleValueChange={handleValueChange}
-            className={className}
-          >
+          <TabComponent options={options} defaultValue={defaultValue} handleValueChange={handleValueChange} className={className}>
             {children}
           </TabComponent>
         ) : (
-          <SelectComponent
-            options={options}
-            defaultValue={defaultValue}
-            handleValueChange={handleValueChange}
-            className={className}
-          />
+          <SelectComponent options={options} defaultValue={defaultValue} handleValueChange={handleValueChange} className={className} />
         )}
       </div>
     </div>
@@ -105,10 +74,7 @@ export const TabsResponsive = ({
 
 interface TabProps {
   defaultValue?: string;
-  handleValueChange: (
-    value: string,
-    option: { href?: string; value?: string }
-  ) => void;
+  handleValueChange: (value: string, option: { href?: string; value?: string }) => void;
   options: {
     name: string;
     href?: string;
@@ -122,46 +88,18 @@ interface TabProps {
   className?: string;
 }
 
-const TabComponent = ({
-  options,
-  defaultValue,
-  handleValueChange,
-  children,
-  fullWidth,
-  className,
-}: TabProps) => {
+const TabComponent = ({ options, defaultValue, handleValueChange, children, fullWidth, className }: TabProps) => {
   return (
     <Tabs defaultValue={defaultValue} className={cn(`w-full`)}>
-      <TabsList
-        className={cn(
-          `w-full`,
-          fullWidth ? " md:w-full" : " md:w-fit",
-          className
-        )}
-      >
+      <TabsList className={cn(`w-full`, fullWidth ? " md:w-full" : " md:w-fit", className)}>
         {options.map((option, index) => (
           <TabsTrigger
             key={index}
             value={option.href ?? option.value ?? ""}
-            className={cn(
-              `w-full flex gap-2`,
-              fullWidth ? " md:w-full" : " md:w-fit"
-            )}
-            onClick={() =>
-              handleValueChange(option.href ?? option.value ?? "", option)
-            }
-          >
+            className={cn(`w-full flex gap-2`, fullWidth ? " md:w-full" : " md:w-fit")}
+            onClick={() => handleValueChange(option.href ?? option.value ?? "", option)}>
             {option.icon} {option.name}
-            {option?.count && option?.count > 0 ? (
-              <Badge
-                className={cn(
-                  "bg-red-400 h-5 w-5 flex items-center justify-center mx-auto",
-                  option.badgeColor
-                )}
-              >
-                {option.count}
-              </Badge>
-            ) : null}
+            {option?.count && option?.count > 0 ? <Badge className={cn("bg-red-400 h-5 w-5 flex items-center justify-center mx-auto", option.badgeColor)}>{option.count}</Badge> : null}
           </TabsTrigger>
         ))}
       </TabsList>
@@ -171,12 +109,7 @@ const TabComponent = ({
   );
 };
 
-const SelectComponent = ({
-  options,
-  defaultValue,
-  handleValueChange,
-  className,
-}: TabProps) => {
+const SelectComponent = ({ options, defaultValue, handleValueChange, className }: TabProps) => {
   const router = useRouter();
   return (
     <div className="w-full md:w-fit">
@@ -190,30 +123,16 @@ const SelectComponent = ({
               value: options.find(option => option.value === value)?.value,
             });
           }
-        }}
-      >
-        <SelectTrigger
-          className={cn(
-            "w-full md:fit font-medium h-12 md:h-fit text-md flex gap-2 items-center",
-            className
-          )}
-        >
+        }}>
+        <SelectTrigger className={cn("w-full md:fit font-medium h-12 md:h-fit text-md flex gap-2 items-center", className)}>
           <SelectValue />
         </SelectTrigger>
         <SelectContent className={cn("font-medium")}>
           {options.map((option, index) => (
-            <SelectItem
-              value={option.href ?? option.value ?? ""}
-              className="w-full md:fit flex gap-2"
-              key={index}
-            >
+            <SelectItem value={option.href ?? option.value ?? ""} className="w-full md:fit flex gap-2" key={index}>
               <div className="flex items-center gap-2">
                 {option.icon} {option.name}
-                {option?.count && option?.count > 0 ? (
-                  <Badge className={cn("bg-red-400", option.badgeColor)}>
-                    {option.count}
-                  </Badge>
-                ) : null}
+                {option?.count && option?.count > 0 ? <Badge className={cn("bg-red-400", option.badgeColor)}>{option.count}</Badge> : null}
               </div>
             </SelectItem>
           ))}
