5302930a2f9fc5f33d81386ab1e93e8d3bd9e757
Vigee <contact@vigee.fr>
2024-08-07 12:06:18 +0200

    variable logo

--- components/Logos/VariableLogo.tsx
+++ components/Logos/VariableLogo.tsx
@@ -1,28 +1,24 @@
-import VigeeGrayLogo from "../../img/logos/VigeeGrayLogo.png"
+import Link from "next/link";
+import VigeeGrayLogo from "../../img/logos/VigeeGrayLogo.png";
+import VigeeWhiteLogo from "../../img/logos/VigeeWhiteLogo.png";
 import Image from "next/image";
+import { cn } from "../lib/utils";
 
 interface VariableLogoProps {
   title?: string;
   big?: boolean;
+  white?: boolean;
+  className?: string;
+  href?: string;
 }
 
-export default function VariableLogo({ title, big }: VariableLogoProps) {
+export default function VariableLogo({ title, big, className, href, white }: VariableLogoProps) {
   return (
-    <div className="flex items-center gap-x-1">
-      <Image
-        src={VigeeGrayLogo}
-        alt="Vigee"
-        width={big ? 40 : 30}
-        height={big ? 40 : 30}
-        className="md:flex mb-1"
-      />
-      <p
-        className={`font-display font-black text-black ${
-          big ? "text-4xl" : "text-2xl"
-        }`}
-      >
-        {title}
-      </p>
-    </div>
+    <Link href={href ?? "/"} className={className}>
+      <div className="flex items-center gap-x-2">
+        <Image src={white ? VigeeWhiteLogo : VigeeGrayLogo} alt="Vigee" width={big ? 400 : 300} height={big ? 400 : 300} className={cn("md:flex mb-1 h-auto", big ? "w-10" : "w-6")} />
+        <p className={cn(`font-display font-black text-black`, big ? "text-4xl" : "text-2xl", white ? "text-gray-100" : "text-black")}>{title}</p>
+      </div>
+    </Link>
   );
 }
