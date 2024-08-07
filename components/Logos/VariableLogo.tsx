9e1145a73650de1eee4a32b6aa7b89a88e75e042
Vigee <contact@vigee.fr>
2024-08-07 12:10:47 +0200

    Variable logo fonts

--- components/Logos/VariableLogo.tsx
+++ components/Logos/VariableLogo.tsx
@@ -17,7 +17,7 @@ export default function VariableLogo({ title, big, className, href, white }: Var
     <Link href={href ?? "/"} className={className}>
       <div className="flex items-center gap-x-2">
         <Image src={white ? VigeeWhiteLogo : VigeeGrayLogo} alt="Vigee" width={big ? 400 : 300} height={big ? 400 : 300} className={cn("md:flex mb-1 h-auto", big ? "w-10" : "w-6")} />
-        <p className={cn(`font-display font-black text-black`, big ? "text-4xl" : "text-2xl", white ? "text-gray-100" : "text-black")}>{title}</p>
+        <p className={cn(` font-variations font-black text-black`, big ? "text-4xl" : "text-2xl", white ? "text-gray-200" : "text-black")}>{title}</p>
       </div>
     </Link>
   );
