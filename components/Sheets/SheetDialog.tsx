b4177c5aabad04fc040c3533cdad0d47ebabd6aa
Vigee <contact@vigee.fr>
2024-08-26 17:29:01 +0200

    fix dynamic height for sheetDialog with view port

--- components/Sheets/SheetDialog.tsx
+++ components/Sheets/SheetDialog.tsx
@@ -2,24 +2,9 @@
 
 import React from "react";
 import { Button } from "../Buttons/Button";
-import {
-  SheetTrigger,
-  SheetTitle,
-  SheetDescription,
-  SheetHeader,
-  SheetClose,
-  SheetContent,
-  Sheet as SheetComponent,
-} from "../ui/sheet";
+import { SheetTrigger, SheetTitle, SheetDescription, SheetHeader, SheetClose, SheetContent, Sheet as SheetComponent } from "../ui/sheet";
 import { useMediaQuery } from "@react-hook/media-query";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "../ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
 
 import { cn } from "../lib/utils";
 
@@ -40,31 +25,17 @@ type SheetContextType = {
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const SheetContext = React.createContext<SheetContextType | undefined>(
-  undefined
-);
+const SheetContext = React.createContext<SheetContextType | undefined>(undefined);
 
 export function useSheetContext() {
   const context = React.useContext(SheetContext);
   if (context === undefined) {
-    throw new Error(
-      "useDrawerContext doit être utilisé à l'intérieur d'un SheetContext.Provider"
-    );
+    throw new Error("useDrawerContext doit être utilisé à l'intérieur d'un SheetContext.Provider");
   }
   return context;
 }
 
-export default function SheetDialog({
-  trigger,
-  title,
-  description,
-  children,
-  side = "bottom",
-  icon,
-  size = "md",
-  scroll = true,
-  sheet = false,
-}: Props) {
+export default function SheetDialog({ trigger, title, description, children, side = "bottom", icon, size = "md", scroll = true, sheet = false }: Props) {
   const [open, setOpen] = React.useState(false);
   const contextValue: SheetContextType = { open, setOpen };
   const isDesktop = useMediaQuery("(min-width: 768px)");
@@ -78,16 +49,7 @@ export default function SheetDialog({
               {trigger}
             </DialogTrigger>
 
-            <DialogContent
-              onClick={e => e.stopPropagation()}
-              className={`max-w-[425px]  ${
-                size === "sm"
-                  ? "md:max-w-[425px]"
-                  : size === "md"
-                  ? "md:max-w-[650px]"
-                  : "md:max-w-[1080px]"
-              } `}
-            >
+            <DialogContent onClick={e => e.stopPropagation()} className={`max-w-[425px]  ${size === "sm" ? "md:max-w-[425px]" : size === "md" ? "md:max-w-[650px]" : "md:max-w-[1080px]"} `}>
               <DialogHeader>
                 <div className="flex items-center gap-x-4 p-4 py-2">
                   {icon}
@@ -98,14 +60,7 @@ export default function SheetDialog({
                 </div>
               </DialogHeader>
 
-              <div
-                className={cn(
-                  "p-1 max-h-[80vh]",
-                  scroll && "overflow-y-scroll"
-                )}
-              >
-                {children}
-              </div>
+              <div className={cn("p-1 max-h-[80vh]", scroll && "overflow-y-scroll")}>{children}</div>
             </DialogContent>
           </Dialog>
         </div>
@@ -120,21 +75,13 @@ export default function SheetDialog({
           {trigger}
         </SheetTrigger>
 
-        <SheetContent
-          side={side}
-          className={`rounded-t-2xl lg:max-w-screen-lg  `}
-          onClick={e => e.stopPropagation()}
-        >
+        <SheetContent side={side} className={`rounded-t-2xl lg:max-w-screen-lg h-lvh overflow-y-scroll`} onClick={e => e.stopPropagation()}>
           <SheetHeader className="text-left flex pb-4">
             <SheetTitle className="text-primary">{title}</SheetTitle>
             <SheetDescription>{description}</SheetDescription>
           </SheetHeader>
 
-          <div
-            className={cn("p-1 max-h-[80vh] ", scroll && "overflow-y-scroll")}
-          >
-            {children}
-          </div>
+          <div className={cn("p-1 ")}>{children}</div>
 
           <SheetClose className="w-full flex justify-center items-center text-center pt-2">
             <Button icon="chevronDown" />
