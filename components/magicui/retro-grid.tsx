cd4e6c117427cc76e3bbd11647ae086b55c9a136
Vigee <contact@vigee.fr>
2024-09-03 10:16:06 +0200

    fix import { cn } from "../lib/utils";

--- components/magicui/retro-grid.tsx
+++ components/magicui/retro-grid.tsx
@@ -1,20 +1,8 @@
-import { cn } from "lib/utils";
+import { cn } from "../lib/utils";
 
-export default function RetroGrid({
-  className,
-  angle = 65,
-}: {
-  className?: string;
-  angle?: number;
-}) {
+export default function RetroGrid({ className, angle = 65 }: { className?: string; angle?: number }) {
   return (
-    <div
-      className={cn(
-        "pointer-events-none absolute size-full overflow-hidden opacity-50 [perspective:200px]",
-        className,
-      )}
-      style={{ "--grid-angle": `${angle}deg` } as React.CSSProperties}
-    >
+    <div className={cn("pointer-events-none absolute size-full overflow-hidden opacity-50 [perspective:200px]", className)} style={{ "--grid-angle": `${angle}deg` } as React.CSSProperties}>
       {/* Grid */}
       <div className="absolute inset-0 [transform:rotateX(var(--grid-angle))]">
         <div
@@ -27,7 +15,7 @@ export default function RetroGrid({
             "[background-image:linear-gradient(to_right,rgba(0,0,0,0.3)_1px,transparent_0),linear-gradient(to_bottom,rgba(0,0,0,0.3)_1px,transparent_0)]",
 
             // Dark styles
-            "dark:[background-image:linear-gradient(to_right,rgba(255,255,255,0.2)_1px,transparent_0),linear-gradient(to_bottom,rgba(255,255,255,0.2)_1px,transparent_0)]",
+            "dark:[background-image:linear-gradient(to_right,rgba(255,255,255,0.2)_1px,transparent_0),linear-gradient(to_bottom,rgba(255,255,255,0.2)_1px,transparent_0)]"
           )}
         />
       </div>
