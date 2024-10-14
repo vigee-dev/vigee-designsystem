e642963353e467436f2d4e83c2398bc38332544e
Vigee <contact@vigee.fr>
2024-10-14 09:40:55 +0200

    added submit button for server axctions

---
+++ components/Buttons/ButtonSubmit.tsx
@@ -0,0 +1,12 @@
+import { useFormStatus } from "react-dom";
+import { Button } from "./Button";
+
+export const ButtonSubmit = () => {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button type="submit" pending={pending}>
+      Enregistrer
+    </Button>
+  );
+};
