d6ba24a4202d354dec1112bd30d3dca9ab06d0ad
Vigee <contact@vigee.fr>
2024-11-01 12:16:43 +0100

    action with validation + button

--- components/Buttons/Button.tsx
+++ components/Buttons/Button.tsx
@@ -184,13 +184,14 @@ const ButtonComponent = ({ children, onClick, variant, type, disabled, className
           <Icon
             className={cn(
               "text-gray-100 transform transition-ease-in-out duration-300 ease-in-out w-5 h-5",
-              className,
+
               !children &&
                 "group-hover:text-primary text-gray-400 group-hover:cursor-pointer hover:text-primary hover:cursor-pointer group-hover:scale-105 transform transition-ease-in-out duration-300 w-6 h-6",
               big && "w-10 h-10",
               variant === "outline" && "text-gray-900",
               variant === "secondary" && "text-secondary-foreground",
-              "group-hover:animate-pulse"
+              "group-hover:animate-pulse",
+              className
             )}
           />
         )}
@@ -211,13 +212,14 @@ const ButtonComponent = ({ children, onClick, variant, type, disabled, className
         <IconLeft
           className={cn(
             "text-gray-100 transform transition-ease-in-out duration-300 ease-in-out w-5 h-5",
-            className,
+
             !children &&
               "group-hover:text-primary text-gray-400 group-hover:cursor-pointer hover:text-primary hover:cursor-pointer group-hover:scale-105 transform transition-ease-in-out duration-300  w-6 h-6",
             big && "w-10 h-10",
             variant === "outline" && "text-gray-900",
             variant === "secondary" && "text-secondary-foreground",
-            "group-hover:animate-pulse"
+            "group-hover:animate-pulse",
+            className
           )}
         />
       )}
@@ -229,13 +231,13 @@ const ButtonComponent = ({ children, onClick, variant, type, disabled, className
         <Icon
           className={cn(
             "text-gray-100 transform transition-ease-in-out duration-300 ease-in-out w-5 h-5",
-
             !children &&
               "group-hover:text-primary text-gray-400 group-hover:cursor-pointer hover:text-primary hover:cursor-pointer group-hover:scale-105 transform transition-ease-in-out duration-300  w-6 h-6",
             big && "w-10 h-10",
             variant === "outline" && "text-gray-900",
             variant === "secondary" && "text-secondary-foreground",
-            "group-hover:animate-pulse"
+            "group-hover:animate-pulse",
+            className
           )}
         />
       )}
--- components/Buttons/DeleteAction.tsx
+++ components/Buttons/DeleteAction.tsx
@@ -1,5 +1,6 @@
 import { AlertDialog } from "../Forms/AlertDialog";
 import { Button } from "../Buttons/Button";
+import { cn } from "../lib/utils";
 
 interface Props {
   alertTitle: string;
@@ -18,6 +19,32 @@ export const DeleteAction = ({ deleteAction, className, buttonText, isPending, a
       onClick={deleteAction}
       trigger={
         <Button icon="trash" variant="outline" className={className}>
+          {buttonText}
+        </Button>
+      }
+      isPending={isPending}
+    />
+  );
+};
+
+interface ActionWithValidationProps {
+  title: string;
+  subtitle?: string;
+  buttonText?: string;
+  className?: string;
+  isPending?: boolean;
+  action: () => void;
+  icon: "trash" | "check" | "cross";
+}
+
+export const ActionWithValidation = ({ action, className, buttonText, isPending, title, subtitle, icon }: ActionWithValidationProps) => {
+  return (
+    <AlertDialog
+      btnQuestion={title || "Êtes-vous sur ?"}
+      btnSubAlert={subtitle || "Cette action est irréversible"}
+      onClick={action}
+      trigger={
+        <Button icon={icon} variant="outline" className={cn(className)}>
           {buttonText}
         </Button>
       }
