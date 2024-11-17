ad07e278e7acfcbd7d37455b72b4c54f9d6081cf
Vigee <contact@vigee.fr>
2024-11-17 18:09:12 +0100

    Update LineContainer.tsx

--- components/Container/LineContainer.tsx
+++ components/Container/LineContainer.tsx
@@ -16,12 +16,12 @@ interface PageHeaderProps {
 }
 
 const Content = ({ title, subtitle, icon, children, className }: Omit<PageHeaderProps, "href" | "onClick">) => (
-  <div className={cn("rounded-xl h-fit px-4 py-4 md:my-4 items-center border-none  hover:bg-zinc-100 transition-ease-in-out duration-100 hover:cursor-pointer bg-transparent md:bg-white ", className)}>
+  <div className={cn("rounded-xl h-fit px-4 py-4 md:my-4 items-center border-none  group transition-ease-in-out duration-100 hover:cursor-pointer bg-transparent md:bg-white ", className)}>
     <div className="flex justify-between  w-full items-center">
       <div className="flex gap-x-10 items-center">
-        {icon && <div className="icon-container text-gray-400">{icon}</div>}
+        {icon && <div className="icon-container text-gray-700 group-hover:text-gray-900">{icon}</div>}
         <div className="flex flex-col px-2">
-          <TypographyH4 className="md:text-base text-sm font-bold">{title}</TypographyH4>
+          <TypographyH4 className="md:text-base text-sm font-bold text-gray-600 group-hover:text-gray-900">{title}</TypographyH4>
           {subtitle && <p className="text-gray-400 md:text-sm text-xs">{subtitle}</p>}
         </div>
       </div>
