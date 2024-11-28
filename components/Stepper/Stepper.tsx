adcfd95103ecde1f78f9d84fea499bf213be39e9
Vigee <contact@vigee.fr>
2024-11-28 10:00:52 +0100

    Update Stepper.tsx

--- components/Stepper/Stepper.tsx
+++ components/Stepper/Stepper.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import { Container } from "./../../components/Container/Container";
 import { PiCheckTickCircleContrast } from "./../../icons/PikaIcons";
+import { cn } from "../../components/lib/utils";
 
 type StepperHook<StepContent extends string> = {
   currentStep: number;
@@ -10,15 +11,12 @@ type StepperHook<StepContent extends string> = {
   prevStep: () => void;
   getCurrentStepContent: () => StepContent;
   steps: StepContent[];
+  className?: string;
 };
 
-function useStepper<StepContent extends string>(
-  initialStep: number,
-  steps: StepContent[]
-) {
+function useStepper<StepContent extends string>(initialStep: number, steps: StepContent[], className?: string) {
   const [currentStep, setCurrentStep] = useState(initialStep);
-  const nextStep = () =>
-    setCurrentStep(prevStep => Math.min(prevStep + 1, steps.length - 1));
+  const nextStep = () => setCurrentStep(prevStep => Math.min(prevStep + 1, steps.length - 1));
   const prevStep = () => setCurrentStep(prevStep => Math.max(prevStep - 1, 0));
   const getCurrentStepContent = () => steps[currentStep];
 
@@ -28,32 +26,20 @@ function useStepper<StepContent extends string>(
     prevStep,
     getCurrentStepContent,
     steps,
+    className,
   };
 }
 
-function Stepper<StepContent extends string>({
-  stepper,
-}: {
-  stepper: StepperHook<StepContent>;
-}) {
+function Stepper<StepContent extends string>({ stepper, className }: { stepper: StepperHook<StepContent>; className?: string }) {
   const complete = stepper.currentStep === stepper.steps.length - 1;
 
   return (
-    <Container className="p-6">
+    <Container className={cn("p-6", className)}>
       <div className="flex justify-between">
         {stepper.steps?.map((step, i) => (
-          <div
-            key={i}
-            className={`step-item ${
-              stepper.currentStep === i ? "active" : ""
-            } ${i < stepper.currentStep ? "complete" : ""}`}
-          >
-            <div className="step">
-              {i < stepper.currentStep ? <PiCheckTickCircleContrast /> : i + 1}
-            </div>
-            <p className="text-gray-500 text-xs md:text-sm font-medium pt-1">
-              {step}
-            </p>
+          <div key={i} className={`step-item ${stepper.currentStep === i ? "active" : ""} ${i < stepper.currentStep ? "complete" : ""}`}>
+            <div className="step">{i < stepper.currentStep ? <PiCheckTickCircleContrast /> : i + 1}</div>
+            <p className="text-gray-500 text-xs md:text-sm font-medium pt-1">{step}</p>
           </div>
         ))}
       </div>
