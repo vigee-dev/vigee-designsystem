b8962d906159ac37d618217c7ad208a504dcc70e
Vigee <contact@vigee.fr>
2024-11-29 09:59:55 +0100

    Update PageHeader.tsx

--- components/Headers/PageHeader.tsx
+++ components/Headers/PageHeader.tsx
@@ -3,7 +3,7 @@ import { cn } from "../lib/utils";
 
 interface PageHeaderProps {
   title: string;
-  subtitle?: string;
+  subtitle?: string | null;
   children?: React.ReactNode;
   small?: boolean;
   icon?: React.ReactNode;
