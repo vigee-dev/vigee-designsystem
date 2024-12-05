c3b3227ca5240e1c59113ae713bdb4b819c0e88d
Vigee <contact@vigee.fr>
2024-12-05 11:20:56 +0100

    Update Toggles.tsx

--- components/Forms/Toggles.tsx
+++ components/Forms/Toggles.tsx
@@ -4,6 +4,7 @@ import { Container } from "../Container/Container";
 import { ToggleGroup as ShadToggleGroup, ToggleGroupItem } from "../ui/toggle-group";
 import { FormControl, FormDescription, FormField, FormItem, FormMessage } from "../ui/form";
 import { cn } from "../lib/utils";
+import { Label } from "../ui/label";
 
 type Option = {
   label?: string;
@@ -15,6 +16,7 @@ type Option = {
 type Props<T extends FieldValues> = {
   form: UseFormReturn<T>;
   name: Path<T>;
+  label?: string;
   descr?: string;
   className?: string;
   optionsClassName?: string;
@@ -22,13 +24,14 @@ type Props<T extends FieldValues> = {
   options: Option[];
 };
 
-export const Toggles = <T extends FieldValues>({ form, name, descr, className, optionsClassName, disabled, options }: Props<T>) => {
+export const Toggles = <T extends FieldValues>({ form, name, label, descr, className, optionsClassName, disabled, options }: Props<T>) => {
   return (
     <FormField
       control={form.control}
       name={name}
       render={({ field }) => (
         <FormItem className="w-full col-span-full">
+          {label && <Label className="text-md font-bold">{label}</Label>}
           <FormControl>
             <Container className={cn("p-1", className)}>
               <ShadToggleGroup type="single" className="w-full flex flex-col md:flex-row" value={field.value} onValueChange={field.onChange} disabled={disabled}>
