7eb503fb9739279440a1f384264698f6dee9e4c0
Vigee <contact@vigee.fr>
2024-12-10 09:19:24 +0100

    Update LineContainer.tsx

--- components/Container/LineContainer.tsx
+++ components/Container/LineContainer.tsx
@@ -18,7 +18,7 @@ interface PageHeaderProps {
 const Content = ({ title, subtitle, icon, children, className }: Omit<PageHeaderProps, "href" | "onClick">) => (
   <div
     className={cn(
-      "rounded-xl h-fit py-2 md:my-2 text-gray-600 hover:text-gray-900 items-center border-none  group transition-ease-in-out duration-100 hover:cursor-pointer bg-transparent transition-ease-in-out duration-100",
+      "rounded-xl h-fit py-2 md:my-2 text-gray-600 hover:text-gray-900 items-center border-none  group transition-ease-in-out duration-100 hover:cursor-pointer bg-transparent transition-ease-in-out ",
       className
     )}>
     <div className="flex justify-between  w-full items-center ">
@@ -26,7 +26,7 @@ const Content = ({ title, subtitle, icon, children, className }: Omit<PageHeader
         {icon && <div className="icon-container   ">{icon}</div>}
         <div className="flex flex-col px-2">
           <TypographyH4 className="text-xs md:text-base  font-bold   ">{title}</TypographyH4>
-          {subtitle && <p className="text-gray-400 group-hover:text-gray-600 md:text-sm text-xs transition-ease-in-out duration-100">{subtitle}</p>}
+          {subtitle && <p className="text-gray-400 group-hover:text-gray-600 md:text-sm text-xs transition-ease-in-out duration-100 max-w-48">{subtitle}</p>}
         </div>
       </div>
       <div className="flex gap-x-4  p-1 ">{children ? children : <PiChevronRightStroke className="text-gray-400 group-hover:text-gray-900 transform ease-in-out duration-300 h-6 w-6" />}</div>
