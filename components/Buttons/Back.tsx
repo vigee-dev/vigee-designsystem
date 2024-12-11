a63faade3eb87d467ed8598c7b04550df02ac315
Vigee <contact@vigee.fr>
2024-12-11 15:20:56 +0100

    Update Back.tsx

--- components/Buttons/Back.tsx
+++ components/Buttons/Back.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import Link from "next/link";
-import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 import { Button } from "./Button";
 import { cn } from "../lib/utils";
+import { useRouter } from "next/navigation";
+import { PiChevronLeftStroke } from "../../icons/PikaIcons";
 
 interface BackProps {
   href?: string;
@@ -9,36 +12,32 @@ interface BackProps {
   onClick?: () => void;
   linkBtn?: boolean;
   className?: string;
+  back?: boolean;
 }
 
-const Back: React.FC<BackProps> = ({ href, where, onClick, linkBtn = false, className }) => {
+const Back: React.FC<BackProps> = ({ href, where, onClick, className, back = false }) => {
+  const router = useRouter();
+
   return (
     <>
       <div className={cn(" flex-grid text-gray-400 hidden md:flex", className)}>
         {href ? (
-          <Link href={href} className="flex flex-grid text-gray-400 pb-1 hover:text-primary transform ease-in-out duration-200 hover:font-bold">
-            <ArrowLeftIcon width={15} />
+          <Link href={href} className="flex flex-grid text-gray-400  hover:text-primary transform ease-in-out duration-200 hover:font-bold items-center">
+            <PiChevronLeftStroke className="w-4 h-4" />
             <p className="mx-1 text-sm "> {where}</p>
           </Link>
-        ) : !linkBtn ? (
-          <Button variant="ghost" onClick={onClick} className="flex flex-grid text-gray-400 pb-1 hover:text-primary transform ease-in-out duration-200 hover:font-bold border-0">
-            <ArrowLeftIcon width={15} />
-            <p className="mx-1 text-sm "> {where}</p>
-          </Button>
         ) : (
-          <Button
-            variant="ghost"
-            onClick={onClick}
-            className="flex flex-grid text-gray-400 pb-1 hover:text-primary transform ease-in-out duration-200 hover:font-bold font-normal gap-0 hover:bg-transparent">
-            <ArrowLeftIcon width={15} />
+          <div onClick={() => router.back()} className="flex flex-grid text-gray-400  hover:text-primary transform ease-in-out duration-200 hover:font-bold items-center hover:cursor-pointer">
+            <PiChevronLeftStroke />
             <p className="mx-1 text-sm "> {where}</p>
-          </Button>
+          </div>
         )}
       </div>
 
       <Button
         icon="chevronLeft"
-        href={href}
+        href={back ? "" : href}
+        onClick={back ? () => router.back() : onClick}
         classNameIcon="bg-zinc-900 text-zinc-50 h-12 w-12 rounded-full bg-zinc-900 p-2 shrink-0 hover:text-zinc-900 hover:bg-zinc-50 z-50"
         className="md:hidden fixed bottom-12 left-0"
       />
