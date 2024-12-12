6e7074e7b13c2adb4fac3433b1daf2a971680550
robinminster <minster.robin.pro@gmail.com>
2024-12-12 12:47:17 +0100

    FIX: issue with content pattern leading to warning when running the project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@
 
 const config = {
   darkMode: ["class"],
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "!./**/node_modules/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}", "./../../apps/**/*.{ts,tsx}"],
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx, js, jsx}", "./app/**/*.{ts,tsx}", "!./**/node_modules/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}", "./../../apps/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
     container: {
