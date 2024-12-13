89a3dc1155b67d3da4f9a655303854851ccfab8c
Vigee <contact@vigee.fr>
2024-12-13 18:01:30 +0100

    Update EmptyIllustration.tsx

--- components/Illustration/EmptyIllustration.tsx
+++ components/Illustration/EmptyIllustration.tsx
@@ -18,9 +18,8 @@ interface IllustrationProps {
 export default function EmptyIllustration({ text, subtitle, buttonLink, buttonText, children, button = false, className }: IllustrationProps) {
   return (
     <Container className={cn("flex flex-col items-center justify-center  border-none bg-transparent w-full px-12 p-8", className)}>
-      <Image width={400} height={400} className="mx-auto w-64 h-auto" src={emptyMan} alt="Empty list" />
-
-      <h2 className={"text-lg text-gray-600 font-medium text-center pt-6 font-display"}>{text}</h2>
+      <Image width={400} height={400} className="mx-auto w-64 h-64" src={emptyMan} alt="Empty list" />
+      <h2 className={"text-lg text-gray-600 font-medium text-center font-display"}>{text}</h2>
       <p className="text-gray-400 text-sm">{subtitle}</p>
 
       {!buttonLink && button && (
