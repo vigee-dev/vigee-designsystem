45f17273e0b9e7114c0a40aa0c762f2842f21bd5
Vigee <contact@vigee.fr>
2024-12-13 18:06:41 +0100

    ERROR : new image for error

--- components/Errors/Error.tsx
+++ components/Errors/Error.tsx
@@ -1,5 +1,6 @@
 import Image, { StaticImageData } from "next/image";
 import error from "../../img/error/error.svg";
+import errorBro from "../../img/error/error-bro.svg";
 import Link from "next/link";
 import { PiEnvelopeArrowRightDuoSolid } from "../../icons/PikaIcons";
 import { Button } from "../Buttons/Button";
@@ -11,11 +12,13 @@ interface IllustrationProps {
   supportEmail?: string;
   img?: StaticImageData;
   onClick?: () => void;
+  variant?: "bro" | "classic";
 }
-export function Error({ title, subtitle, children, supportEmail, onClick, img = error }: IllustrationProps) {
+export function Error({ title, subtitle, children, supportEmail, onClick, variant = "bro" }: IllustrationProps) {
+  const image = variant === "bro" ? errorBro : error;
   return (
     <div className="flex flex-col items-center justify-center  w-full px-12 max-w-2xl">
-      {img && <Image width={400} height={400} className="mx-auto w-44 h-auto" src={img} alt="Erreur" />}
+      <Image width={400} height={400} className="mx-auto w-44 h-auto" src={image} alt="Erreur" />
       <h1 className={"text-xl text-primary font-bold text-center pt-6 font-display"}>{title ? title : "Une erreur s'est produite."}</h1>
 
       {subtitle && <p className="text-gray-400">{subtitle}</p>}
