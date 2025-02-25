2d55b93b630ff55fc81a5ad6fed220b71d8592a0
robinminster <minster.robin.pro@gmail.com>
2025-02-25 19:38:10 +0100

    UPDATE: implemented classname to Spinner.tsx

--- components/Loaders/Spinner.tsx
+++ components/Loaders/Spinner.tsx
@@ -1,3 +1,3 @@
-export const Spinner = () => {
-  return <span className="w-7 h-7 border-[5px]   animate-[spin_2s_linear_infinite]  border-t-[5px] border-t-solid border-t-primary  rounded-full transition-transform  ease-in-outout z-50" />;
+export const Spinner = ({className}: { className?: string }) => {
+  return <span className={`w-7 h-7 border-[5px]   animate-[spin_2s_linear_infinite]  border-t-[5px] border-t-solid border-t-primary  rounded-full transition-transform  ease-in-outout z-50 ${className}`} />;
 };
