ee2288e3fca25094713732d4214e61cee963d405
Vigee <contact@vigee.fr>
2025-03-17 16:54:18 +0100

    new body typo

--- components/Drawer/Drawer.tsx
+++ components/Drawer/Drawer.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "components/lib/utils";
 import { Drawer as VaulDrawer } from "vaul";
 
 export function Drawer({
@@ -10,6 +11,7 @@ export function Drawer({
   open,
   onClose,
   onOpenChange,
+  className,
 }: {
   children: React.ReactNode;
   trigger: React.ReactNode;
@@ -18,13 +20,14 @@ export function Drawer({
   open?: boolean;
   onClose?: () => void;
   onOpenChange?: (open: boolean) => void;
+  className?: string;
 }) {
   return (
     <VaulDrawer.Root open={open} onClose={onClose} onOpenChange={onOpenChange}>
       <VaulDrawer.Trigger className="">{trigger}</VaulDrawer.Trigger>
       <VaulDrawer.Portal>
         <VaulDrawer.Overlay className="fixed inset-0 bg-black/40" />
-        <VaulDrawer.Content className="bg-white flex flex-col fixed z-50 bottom-0 left-0 right-0 max-h-[92vh] rounded-t-[10px]">
+        <VaulDrawer.Content className={cn("bg-white flex flex-col fixed z-50 bottom-0 left-0 right-0 max-h-[92vh] rounded-t-[10px]", className)}>
           <div className="max-w-md w-full mx-auto overflow-auto p-4 rounded-t-[10px]">
             <VaulDrawer.Handle />
             <VaulDrawer.Title className="font-bold text-gray-900 mt-8 text-lg">{title}</VaulDrawer.Title>
