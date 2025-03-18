9e1df1fbf72aac2e273106ae84592f2bf57e78cc
Vigee <contact@vigee.fr>
2025-03-18 09:17:42 +0100

    sidebar props

--- components/Container/LineContainer.tsx
+++ components/Container/LineContainer.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { TypographyH4 } from "../Typography/Typography";
-import { PiChevronBigRightStroke, PiChevronRightStroke } from "../../icons/PikaIcons";
+import { PiChevronRightStroke } from "../../icons/PikaIcons";
 import { cn } from "../lib/utils";
 
 interface PageHeaderProps {
@@ -18,7 +18,7 @@ interface PageHeaderProps {
 const Content = ({ title, subtitle, icon, children, className }: Omit<PageHeaderProps, "href" | "onClick">) => (
   <div
     className={cn(
-      "rounded-xl h-fit py-2 md:my-2 text-gray-600 hover:text-gray-900 items-center border-none  group transition-ease-in-out duration-100 hover:cursor-pointer bg-transparent transition-ease-in-out ",
+      "rounded-xl h-fit py-2 md:my-2 text-primary hover:opacity-80 items-center border-none  group transition-ease-in-out duration-100 hover:cursor-pointer bg-transparent transition-ease-in-out ",
       className
     )}>
     <div className="flex justify-between  w-full items-center ">
@@ -26,10 +26,10 @@ const Content = ({ title, subtitle, icon, children, className }: Omit<PageHeader
         {icon && <div className="icon-container   ">{icon}</div>}
         <div className="flex flex-col px-2">
           <TypographyH4 className="text-xs md:text-base  font-bold   ">{title}</TypographyH4>
-          {subtitle && <p className="text-gray-400 group-hover:text-gray-600 md:text-sm text-xs transition-ease-in-out duration-100 max-w-48 md:max-w-none">{subtitle}</p>}
+          {subtitle && <p className=" md:text-sm text-xs transition-ease-in-out duration-100 max-w-48 md:max-w-none">{subtitle}</p>}
         </div>
       </div>
-      <div className="flex gap-x-4  p-1 ">{children ? children : <PiChevronRightStroke className="text-gray-400 group-hover:text-gray-900 transform ease-in-out duration-300 h-6 w-6" />}</div>
+      <div className="flex gap-x-4  p-1 ">{children ? children : <PiChevronRightStroke className="text-primary group-hover:text-primary-light transform ease-in-out duration-300 h-6 w-6" />}</div>
     </div>
   </div>
 );
