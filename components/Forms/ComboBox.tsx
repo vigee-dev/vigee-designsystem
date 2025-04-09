9bab49cd56a158a346aab164cd7acd0c3b916767
Vigee <contact@vigee.fr>
2025-04-09 11:51:46 +0200

    new toggle style withcheckbox

--- components/Forms/ComboBox.tsx
+++ components/Forms/ComboBox.tsx
@@ -4,21 +4,10 @@ import React from "react";
 import { UseFormReturn, FieldValues, Path } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "../../components/ui/button";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-} from "../../components/ui/command";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "../../components/ui/popover";
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem } from "../../components/ui/command";
+import { Popover, PopoverContent, PopoverTrigger } from "../../components/ui/popover";
 import { Check, ChevronsUpDown } from "lucide-react";
 import { cn } from "../lib/utils";
-import { Label } from "../ui/label";
 import { FormLabel } from "../ui/form";
 import { ScrollArea } from "../ui/scroll-area";
 
@@ -38,16 +27,7 @@ type ComboBoxProps<T extends z.ZodType<any, any>> = {
   onChange: (value: string | undefined) => void;
 };
 
-export function ComboBox<T extends z.ZodType<any, any, any>>({
-  items,
-  form,
-  name,
-  label,
-  placeholder = "Sélectionnez...",
-  required = true,
-  icon,
-  onChange,
-}: ComboBoxProps<T>) {
+export function ComboBox<T extends z.ZodType<any, any, any>>({ items, form, name, label, placeholder = "Sélectionnez...", required = true, icon, onChange }: ComboBoxProps<T>) {
   let value: string | undefined;
 
   if (form) {
@@ -58,30 +38,19 @@ export function ComboBox<T extends z.ZodType<any, any, any>>({
 
   // Fonction de filtrage personnalisée
   const filterItems = (label = "", search = "") => {
-    // Votre logique de filtrage ici, retournez 1 pour un match, 0 sinon.
-    // Par exemple, filtrer sur le label des items
     if (label.toLowerCase().includes(search.toLowerCase())) return 1;
     return 0;
   };
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
-      {label && (
-        <FormLabel className="font-black text-primary pb-2">{label}</FormLabel>
-      )}
+      {label && <FormLabel className="font-black text-primary pb-2">{label}</FormLabel>}
 
       <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-full flex gap-x-2 bg-input border-0 justify-between"
-        >
+        <Button variant="outline" role="combobox" aria-expanded={open} className="w-full flex gap-x-2 bg-input border-0 justify-between">
           <div className="flex gap-x-2 items-center">
             {icon && icon}
-            {value
-              ? items.find(item => item.value === value)?.label
-              : placeholder}
+            {value ? items.find(item => item.value === value)?.label : placeholder}
           </div>
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
@@ -98,25 +67,15 @@ export function ComboBox<T extends z.ZodType<any, any, any>>({
                   key={item.value}
                   value={item.value}
                   onSelect={(currentValue: string) => {
-                    const valueToUpdate =
-                      currentValue === value ? undefined : currentValue;
+                    const valueToUpdate = currentValue === value ? undefined : currentValue;
 
                     if (form) {
-                      form.setValue(
-                        name as Path<z.infer<T> & FieldValues>,
-                        valueToUpdate as any
-                      );
+                      form.setValue(name as Path<z.infer<T> & FieldValues>, valueToUpdate as any);
                     }
 
                     setOpen(false);
-                  }}
-                >
-                  <Check
-                    className={cn(
-                      "mr-2 h-4 w-4",
-                      value === item.value ? "opacity-100" : "opacity-0"
-                    )}
-                  />
+                  }}>
+                  <Check className={cn("mr-2 h-4 w-4", value === item.value ? "opacity-100" : "opacity-0")} />
                   {item.label}
                 </CommandItem>
               ))}
--- components/Forms/Toggles.tsx
+++ components/Forms/Toggles.tsx
@@ -5,6 +5,8 @@ import { ToggleGroup as ShadToggleGroup, ToggleGroupItem } from "../ui/toggle-gr
 import { FormControl, FormDescription, FormField, FormItem, FormMessage } from "../ui/form";
 import { cn } from "../lib/utils";
 import { Label } from "../ui/label";
+import { motion } from "framer-motion";
+import { PiCheckTickCircleDuoSolid } from "../../icons/PikaIcons";
 
 type Option = {
   label?: string;
@@ -27,6 +29,7 @@ type Props<T extends FieldValues> = {
   multi?: boolean;
   variant?: "default" | "small";
   onChange?: (value: string) => void;
+  withCheckbox?: boolean;
 };
 
 export const Toggles = <T extends FieldValues>({
@@ -42,6 +45,7 @@ export const Toggles = <T extends FieldValues>({
   multi = false,
   variant = "default",
   onChange,
+  withCheckbox = false,
 }: Props<T>) => {
   const handleValueChange = React.useCallback(
     (value: string | string[]) => {
@@ -64,29 +68,37 @@ export const Toggles = <T extends FieldValues>({
             <Container className={cn("p-1", className, variant === "small" && "border-none")}>
               <ShadToggleGroup
                 type={multi ? "multiple" : "single"}
-                className={cn(`w-full flex flex-col md:flex-row  flex-wrap`, nowrap && "flex-nowrap", variant === "small" && "flex-nowrap border-none w-fit")}
+                className={cn(`w-full flex flex-col md:flex-row flex-wrap`, nowrap && "flex-nowrap", variant === "small" && "flex-nowrap border-none w-full")}
                 value={field.value}
                 onValueChange={field.onChange}
                 disabled={disabled}>
                 {options.map(option => (
-                  <ToggleGroupItem
-                    key={option.value}
-                    value={option.value}
-                    aria-label={option.label || ""}
-                    disabled={option.disabled}
-                    className={cn(
-                      " items-center h-fit p-4 flex justify-between w-full gap-6 md:px-6 rounded-lg  border-none data-[state=on]:bg-input hover:bg-input",
-                      optionsClassName,
-                      variant === "small" && "rounded-full border-none p-2 gap-0 justify-center data-[state=on]:bg-primary data-[state=on]:text-white md:px-3"
-                    )}>
-                    <div className="w-fit">{option.icon}</div>
-                    {(option.label || option.description) && (
-                      <div className="w-full text-left ">
-                        {option.label && <span className="text-md font-bold ">{option.label}</span>}
-                        {option.description && <p className="text-xs text-gray-500">{option.description}</p>}
+                  <motion.div key={option.value} initial={{ opacity: 0.8, scale: 0.95 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.3 }} className="w-full">
+                    <ToggleGroupItem
+                      value={option.value}
+                      aria-label={option.label || ""}
+                      disabled={option.disabled}
+                      className={cn(
+                        "items-center h-fit p-4 flex justify-between w-full gap-6 md:px-6 rounded-lg border-none data-[state=on]:bg-input hover:bg-input",
+                        optionsClassName,
+                        variant === "small" && "rounded-full border-none p-2 gap-0 justify-center  data-[state=on]:bg-primary data-[state=on]:text-white md:px-3"
+                      )}>
+                      <div className="flex items-center w-full gap-4">
+                        <div>{option.icon}</div>
+                        {(option.label || option.description) && (
+                          <div className="w-full text-left">
+                            {option.label && <span className="text-md font-bold">{option.label}</span>}
+                            {option.description && <p className="text-xs text-gray-500">{option.description}</p>}
+                          </div>
+                        )}
                       </div>
-                    )}
-                  </ToggleGroupItem>
+                      {withCheckbox && field.value === option.value && (
+                        <motion.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.3 }} className="ml-2">
+                          <PiCheckTickCircleDuoSolid className="text-primary" />
+                        </motion.div>
+                      )}
+                    </ToggleGroupItem>
+                  </motion.div>
                 ))}
               </ShadToggleGroup>
             </Container>
