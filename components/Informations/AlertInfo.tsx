fcb005de001a7cec598c682cc044b4f3176672ca
Romain Dagnan <50986114+vigee-dev@users.noreply.github.com>
2025-04-11 22:20:47 +0200

    fix AlertTitle

--- components/Informations/AlertInfo.tsx
+++ components/Informations/AlertInfo.tsx
@@ -19,7 +19,13 @@ export function AlertInfo({
 }: {
   title: string;
   description?: string;
-  type: "neutral" | "informative" | "destructive" | "warning" | "question" | "success";
+  type:
+    | "neutral"
+    | "informative"
+    | "destructive"
+    | "warning"
+    | "question"
+    | "success";
   className?: string;
   link?: string;
 }) {
@@ -27,22 +33,22 @@ export function AlertInfo({
     type === "informative"
       ? "text-informative bg-informative-background"
       : type === "destructive"
-      ? "text-destructive-foreground bg-destructive-background"
-      : type === "warning"
-      ? "text-warning-foreground bg-warning-background"
-      : type === "success"
-      ? "text-success-foreground bg-success-background"
-      : "text-neutral-foreground bg-neutral-background";
+        ? "text-destructive bg-destructive-background"
+        : type === "warning"
+          ? "text-warning-foreground bg-warning-background"
+          : type === "success"
+            ? "text-success-foreground bg-success-background"
+            : "text-neutral-foreground bg-neutral-background";
   const lightColor =
     type === "informative"
       ? "text-informative-foreground-light "
       : type === "destructive"
-      ? "text-destructive-foreground-light "
-      : type === "warning"
-      ? "text-warning-foreground-light "
-      : type === "success"
-      ? "text-success-foreground-light "
-      : "text-neutral-foreground-light ";
+        ? "text-destructive-foreground-light "
+        : type === "warning"
+          ? "text-warning-foreground-light "
+          : type === "success"
+            ? "text-success-foreground-light "
+            : "text-neutral-foreground-light ";
 
   const icon =
     type === "informative" ? (
@@ -58,12 +64,24 @@ export function AlertInfo({
     );
 
   return (
-    <Alert className={cn("flex items-center p-3 rounded-xl border-none", color, className)}>
+    <Alert
+      className={cn(
+        "flex items-center p-3 rounded-xl border-none",
+        color,
+        className
+      )}
+    >
       <div className="flex items-center gap-4">
         {icon}
         <div className="flex flex-col">
-          <AlertTitle className={cn(color, "text-sm py-0 my-0")}>{title}</AlertTitle>
-          {description && <AlertDescription className={cn(lightColor, "text-xs")}>{description}</AlertDescription>}
+          <AlertTitle className={cn(color, "text-sm py-0 my-0")}>
+            {title}
+          </AlertTitle>
+          {description && (
+            <AlertDescription className={cn(lightColor, "text-xs")}>
+              {description}
+            </AlertDescription>
+          )}
         </div>
         {link && (
           <Link href={link}>
