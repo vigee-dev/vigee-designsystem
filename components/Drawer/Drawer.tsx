a5f7ed37d6bd3bb0f7a430113e1a3e5da8ebde8a
Romain Dagnan <50986114+vigee-dev@users.noreply.github.com>
2025-05-06 09:14:14 +0200

    fix drawer

--- components/Drawer/Drawer.tsx
+++ components/Drawer/Drawer.tsx
@@ -52,32 +52,34 @@ export function Drawer({
             fullScreen && "min-h-dvh rounded-none bg-background"
           )}
         >
-          <div
-            className={cn(
-              `max-w-lg w-full mx-auto overflow-auto p-4 rounded-t-[10px]`,
-              classNameContent,
-              fullScreen && "rounded-none"
-            )}
-          >
-            <VaulDrawer.Handle />
-            {cancelable && <Back where="retour" onClick={onClose} />}
-            <div className="flex items-center gap-4">
-              {icon}
-              {title || description ? (
-                <div className="flex justify-between w-full">
-                  <div className="flex flex-col">
-                    <VaulDrawer.Title className="font-bold text-gray-900 mt-8 text-lg">
-                      {title}
-                    </VaulDrawer.Title>
-                    <VaulDrawer.Description className="leading-6  text-gray-600 pb-4">
-                      {description}
-                    </VaulDrawer.Description>
+          <div className="w-full h-full overflow-auto">
+            <div
+              className={cn(
+                "max-w-lg w-full mx-auto p-4 rounded-t-[10px]",
+                classNameContent,
+                fullScreen && "rounded-none"
+              )}
+            >
+              <VaulDrawer.Handle />
+              {cancelable && <Back where="retour" onClick={onClose} />}
+              <div className="flex items-center gap-4">
+                {icon}
+                {title || description ? (
+                  <div className="flex justify-between w-full">
+                    <div className="flex flex-col">
+                      <VaulDrawer.Title className="font-bold text-gray-900 mt-8 text-lg">
+                        {title}
+                      </VaulDrawer.Title>
+                      <VaulDrawer.Description className="leading-6  text-gray-600 pb-4">
+                        {description}
+                      </VaulDrawer.Description>
+                    </div>
+                    {headerComponent}
                   </div>
-                  {headerComponent}
-                </div>
-              ) : null}
+                ) : null}
+              </div>
+              {children}
             </div>
-            {children}
           </div>
         </VaulDrawer.Content>
       </VaulDrawer.Portal>
