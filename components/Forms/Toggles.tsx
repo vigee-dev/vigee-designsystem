aa213fceb9b85707e39b8a594b0703c4db451b3a
Romain Dagnan <50986114+vigee-dev@users.noreply.github.com>
2025-05-06 10:13:15 +0200

    fix drawer

--- components/Forms/Toggles.tsx
+++ components/Forms/Toggles.tsx
@@ -70,88 +70,92 @@ export const Toggles = <T extends FieldValues>({
     <FormField
       control={form.control}
       name={name}
-      render={({ field }) => (
-        <FormItem className="w-full col-span-full">
-          {label && <Label className="font-bold">{label}</Label>}
-          <FormControl>
-            <Container
-              className={cn(
-                "p-1",
-                className,
-                variant === "small" && "border-none"
-              )}
-            >
-              <ShadToggleGroup
-                type={multi ? "multiple" : "single"}
+      render={({ field }) => {
+        const isNowrap = !!nowrap;
+        const gridColsClass = !isNowrap ? `grid-cols-${options.length}` : "";
+        return (
+          <FormItem className="w-full col-span-full">
+            {label && <Label className="font-bold">{label}</Label>}
+            <FormControl>
+              <Container
                 className={cn(
-                  "w-full grid md:grid-cols-3 gap-2",
-                  nowrap && "flex-nowrap",
-                  variant === "small" && "border-none w-full"
+                  "p-1",
+                  className,
+                  variant === "small" && "border-none"
                 )}
-                value={field.value}
-                onValueChange={field.onChange}
-                disabled={disabled}
               >
-                {options.map((option) => (
-                  <motion.div
-                    key={option.value}
-                    initial={{ opacity: 0.8, scale: 0.95 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 0.3 }}
-                    className="h-full"
-                  >
-                    <ToggleGroupItem
-                      value={option.value}
-                      aria-label={option.label || ""}
-                      disabled={option.disabled}
-                      className={cn(
-                        "items-center h-full p-4 flex justify-between w-full gap-4 md:px-6 rounded-lg border-none data-[state=on]:bg-input hover:bg-input",
-                        optionsClassName,
-                        variant === "small" &&
-                          "rounded-full border-none p-2 gap-0 justify-center  data-[state=on]:bg-primary data-[state=on]:text-white md:px-3"
-                      )}
+                <ShadToggleGroup
+                  type={multi ? "multiple" : "single"}
+                  className={cn(
+                    "w-full gap-2",
+                    isNowrap ? "flex" : `grid ${gridColsClass}`,
+                    variant === "small" && "border-none w-full"
+                  )}
+                  value={field.value}
+                  onValueChange={field.onChange}
+                  disabled={disabled}
+                >
+                  {options.map((option) => (
+                    <motion.div
+                      key={option.value}
+                      initial={{ opacity: 0.8, scale: 0.95 }}
+                      animate={{ opacity: 1, scale: 1 }}
+                      transition={{ duration: 0.3 }}
+                      className={cn("h-full w-full", isNowrap && "flex-1")}
                     >
-                      <div className="flex items-center w-full gap-4">
-                        <div>{option.icon}</div>
-                        {(option.label || option.description) && (
-                          <div className="w-full text-left">
-                            {option.label && (
-                              <span className="text-md font-bold">
-                                {option.label}
-                              </span>
-                            )}
-                            {option.description && (
-                              <p className="text-xs text-gray-500">
-                                {option.description}
-                              </p>
-                            )}
-                          </div>
+                      <ToggleGroupItem
+                        value={option.value}
+                        aria-label={option.label || ""}
+                        disabled={option.disabled}
+                        className={cn(
+                          "items-center h-full w-full p-4 flex justify-between gap-4 md:px-6 rounded-lg border-none data-[state=on]:bg-input hover:bg-input",
+                          optionsClassName,
+                          variant === "small" &&
+                            "rounded-full border-none p-2 gap-0 justify-center  data-[state=on]:bg-primary data-[state=on]:text-white md:px-3"
                         )}
-                      </div>
-                      {withCheckbox &&
-                        (multi
-                          ? Array.isArray(field.value) &&
-                            field.value.includes(option.value)
-                          : field.value === option.value) && (
-                          <motion.div
-                            initial={{ opacity: 0, scale: 0.5 }}
-                            animate={{ opacity: 1, scale: 1 }}
-                            transition={{ duration: 0.3 }}
-                            className="ml-2"
-                          >
-                            <PiCheckTickSquareDuoSolid className="text-success-foreground" />
-                          </motion.div>
-                        )}
-                    </ToggleGroupItem>
-                  </motion.div>
-                ))}
-              </ShadToggleGroup>
-            </Container>
-          </FormControl>
-          {descr && <FormDescription>{descr}</FormDescription>}
-          <FormMessage />
-        </FormItem>
-      )}
+                      >
+                        <div className="flex items-center w-full gap-4">
+                          <div>{option.icon}</div>
+                          {(option.label || option.description) && (
+                            <div className="w-full text-left">
+                              {option.label && (
+                                <span className="text-md font-bold">
+                                  {option.label}
+                                </span>
+                              )}
+                              {option.description && (
+                                <p className="text-xs text-gray-500">
+                                  {option.description}
+                                </p>
+                              )}
+                            </div>
+                          )}
+                        </div>
+                        {withCheckbox &&
+                          (multi
+                            ? Array.isArray(field.value) &&
+                              field.value.includes(option.value)
+                            : field.value === option.value) && (
+                            <motion.div
+                              initial={{ opacity: 0, scale: 0.5 }}
+                              animate={{ opacity: 1, scale: 1 }}
+                              transition={{ duration: 0.3 }}
+                              className="ml-2"
+                            >
+                              <PiCheckTickSquareDuoSolid className="text-success-foreground" />
+                            </motion.div>
+                          )}
+                      </ToggleGroupItem>
+                    </motion.div>
+                  ))}
+                </ShadToggleGroup>
+              </Container>
+            </FormControl>
+            {descr && <FormDescription>{descr}</FormDescription>}
+            <FormMessage />
+          </FormItem>
+        );
+      }}
     />
   );
 };
