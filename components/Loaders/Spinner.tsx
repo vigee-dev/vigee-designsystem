6dbd4575d42014a7235cd15e045ee79df37a51be
robinminster <minster.robin.pro@gmail.com>
2025-07-03 17:10:42 +0200

    UPDATE: Updated Spinner.tsx to handle custom classNames

--- components/Loaders/Spinner.tsx
+++ components/Loaders/Spinner.tsx
@@ -1,3 +1,5 @@
-export const Spinner = () => {
-  return <span className="w-7 h-7 border-[5px]   animate-[spin_2s_linear_infinite]  border-t-[5px] border-t-solid border-t-primary  rounded-full transition-transform  ease-in-outout z-50" />;
+import { cn } from "../lib/utils";
+
+export const Spinner = ({ className }: { className?: string }) => {
+  return <span className={cn("w-6 h-6 border-[5px]   animate-[spin_2s_linear_infinite]  border-t-[5px] border-t-solid border-t-primary  rounded-full transition-transform  ease-in-outout z-50", className)} />;
 };
