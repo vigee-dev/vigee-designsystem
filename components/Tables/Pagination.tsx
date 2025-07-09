433147bf35718a59bfaef8f7e24142bd6fb11a02
Julien <35800390+JulienEmrot@users.noreply.github.com>
2025-07-09 08:46:05 +0200

    add param to Pagination.tsx

--- components/Tables/Pagination.tsx
+++ components/Tables/Pagination.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { generatePagination } from "../lib/utils";
 import { usePathname, useSearchParams } from "next/navigation";
 
-export default function Pagination({ totalPages, paramName = "page" }: { totalPages: number, paramName: string }) {
+export default function Pagination({ totalPages, paramName = "page" }: { totalPages: number, paramName?: string }) {
   // NOTE: comment in this code when you get to this point in the course
   const pathname = usePathname();
   const searchParams = useSearchParams();
