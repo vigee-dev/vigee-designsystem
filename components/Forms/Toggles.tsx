a969a7efd5b1d46cd98126d0a7c07e1e4a9240ad
Peter Lesouchu <peterlesouchu@macbook-pro-de-robin.home>
2025-10-14 10:32:50 +0200

    fix : add style props on motion div instead className

--- components/Forms/Toggles.tsx
+++ components/Forms/Toggles.tsx
@@ -222,7 +222,12 @@ export const Toggles = <T extends FieldValues>({
                       initial={{ opacity: 0.8, scale: 0.95 }}
                       animate={{ opacity: 1, scale: 1 }}
                       transition={{ duration: 0.3 }}
-                      className={cn("h-full w-full", isNowrap && "flex-1")}
+                      // Probleme de version entre react etla lib motion, on utilise le style pour eviter l'erreur TS avec la prop className
+                      style={{
+                        height: "100%",
+                        width: "100%",
+                        flex: isNowrap ? "1" : undefined,
+                      }}
                     >
                       <ToggleGroupItem
                         value={option.value}
@@ -261,7 +266,7 @@ export const Toggles = <T extends FieldValues>({
                               initial={{ opacity: 0, scale: 0.5 }}
                               animate={{ opacity: 1, scale: 1 }}
                               transition={{ duration: 0.3 }}
-                              className="ml-2"
+                              style={{ marginLeft: "0.5rem" }}
                             >
                               <PiCheckTickSquareDuoSolid className="text-success-foreground" />
                             </motion.div>
