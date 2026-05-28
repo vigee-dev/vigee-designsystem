deea4d703b79d7d36d6c48cf009840092fb39c76
Vigee <50986114+vigee-dev@users.noreply.github.com>
2026-05-28 12:26:17 +0200

    ui(viewer): ImageView applique width sur le conteneur (largeur réelle respectée)

--- components/DocumentViewer/parts/ImageView.tsx
+++ components/DocumentViewer/parts/ImageView.tsx
@@ -14,12 +14,14 @@ export interface ImageViewProps {
 
 export function ImageView({ url, alt, width }: ImageViewProps) {
   return (
-    <div className="bg-white rounded-md shadow-xl shadow-gray-300/50 ring-1 ring-gray-200 overflow-hidden">
+    <div
+      style={{ width }}
+      className="bg-white rounded-md shadow-xl shadow-gray-300/50 ring-1 ring-gray-200 overflow-hidden"
+    >
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         src={url}
         alt={alt}
-        style={{ maxWidth: width }}
         className="block w-full h-auto"
       />
     </div>
